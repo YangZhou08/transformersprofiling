fc670a2513467e83cb2cb53aa45985e98da588ae
YangZhou08 <yangzhou25672@utexas.edu>
2023-10-05 00:30:41 -0400

    newprint

--- main.py
+++ main.py
@@ -14,3 +14,7 @@ def run():
     tokenizer = T5Tokenizer.from_pretrained("google/mt5-small") # TODO: need a better solution 
     
     small_model = AutoModelForSeq2SeqLM.from_pretrained("t5-small").to(torch_device) 
+    
+    input_ids = tokenizer.encode("I am new to huggingface transformers", return_tensors = "pt").to(torch_device) 
+    
+    small_model(input_ids) 
