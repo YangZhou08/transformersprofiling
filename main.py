df0bc064089e1cdb7743ebe7d5c52783480be4e5
YangZhou08 <yangzhou25672@utexas.edu>
2023-10-05 00:40:06 -0400

    srctransfor

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import torch
 import argparse 
 # import contexttimer 
 
-from transformers import AutoTokenizer, AutoModelForCausalLM, AutoModelForSeq2SeqLM 
+from src.transformers import AutoTokenizer, AutoModelForCausalLM, AutoModelForSeq2SeqLM 
+from src.transformers import FlaxT5EncoderModel, T5Tokenizer 
 
 from tqdm import tqdm
 from sampling.utils import norm_logits, sample
@@ -10,7 +11,7 @@ from sampling.utils import norm_logits, sample
 def run(): 
     torch_device = 'cuda' if torch.cuda.is_available() else 'cpu' 
     
-    from transformers import FlaxT5EncoderModel, T5Tokenizer
+    # from transformers import FlaxT5EncoderModel, T5Tokenizer 
     tokenizer = T5Tokenizer.from_pretrained("google/mt5-small") # TODO: need a better solution 
     
     small_model = AutoModelForSeq2SeqLM.from_pretrained("t5-small").to(torch_device) 
