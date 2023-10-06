e3aa34ab690dd116616715b01e69f498e5072707
YangZhou08 <yangzhou25672@utexas.edu>
2023-10-06 13:55:53 -0400

    changeo

--- main.py
+++ main.py
@@ -89,6 +89,7 @@ def run():
     small_model = AutoModelForSeq2SeqLM.from_pretrained("t5-3b", cache_dir = "/rscratch/zhendong/yang_tasc").to(torch_device) 
     small_model.eval() 
     
+    word_prefix = "summarize: " 
     word_seq = "I am new to huggingface transformers" 
     word_seq = "Peter want to marry a German woman" 
     word_seq = "I am a student." 
@@ -96,8 +97,8 @@ def run():
     # word_seq = "Four score and seven years ago our fathers brought forth on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal. Now we are engaged in a great civil war, testing whether that nation, or any nation so conceived and so dedicated, can long endure. We are met on a great battlefield of that war. We have come to dedicate a portion of that field, as a final resting place for those who here gave their lives that that nation might live." 
     # word_seq = "We the People of the United States, in Order to form a more perfect Union, establish Justice, insure domestic Tranquility, provide for the common defence, promote the general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do ordain and establish this Constitution for the United States of America." 
     word_seq = "The Apollo 11 mission in 1969 marked a monumental achievement for humanity. American astronauts Neil Armstrong and Buzz Aldrin became the first humans to walk on the moon, with Armstrong's famous words: 'That's one small step for man, one giant leap for mankind." 
-    word_suffix = " In the previous sentence, what did Neil Armstrong say?" 
-    word_seq = word_seq + word_suffix 
+    # word_suffix = " In the previous sentence, what did Neil Armstrong say?" 
+    word_seq = word_prefix + word_seq 
     
     input_ids = tokenizer.encode(word_seq, return_tensors = "pt").to(torch_device) 
     
