f402be50d02e27714eb4a3423685773fa2d0dd52
YangZhou08 <yangzhou25672@utexas.edu>
2023-10-07 00:14:31 -0400

    t5small

--- operation_flops_memory_profiling.py
+++ operation_flops_memory_profiling.py
@@ -83,7 +83,7 @@ def run():
     # torch_device = 'cpu' 
     
     # from transformers import FlaxT5EncoderModel, T5Tokenizer 
-    tokenizer = AutoTokenizer("t5-small", cache_dir = "/rscratch/zhendong/yang_tasc") 
+    tokenizer = AutoTokenizer.from_pretrained("t5-small", cache_dir = "/rscratch/zhendong/yang_tasc") 
     # tokenizer = AutoTokenizer.from_pretrained("t5-3b", cache_dir = "/rscratch/zhendong/yang_tasc") 
     
     # tokenizer = T5Tokenizer.from_pretrained("google/mt5-small") # TODO: need a better solution 
