61e9d120eef9c689b9125157df5caba93751ba87
YangZhou08 <yangzhou25672@utexas.edu>
2023-10-19 16:19:40 -0700

    pythia

--- simple_test_script.py
+++ simple_test_script.py
@@ -4,6 +4,7 @@ import argparse
 
 from src.transformers import AutoTokenizer, AutoModelForCausalLM, AutoModelForSeq2SeqLM 
 from src.transformers import FlaxT5EncoderModel, T5Tokenizer, T5Config 
+from src.transformers import GPTNeoXForCausalLM 
 
 from tqdm import tqdm
 # from sampling.utils import norm_logits, sample 
@@ -86,26 +87,28 @@ def run():
     
     # from transformers import FlaxT5EncoderModel, T5Tokenizer 
     # tokenizer = AutoTokenizer("t5-small", trust_remote_code = True) 
-    tokenizer = AutoTokenizer.from_pretrained("t5-small", cache_dir = "/rscratch/zhendong/yang_tasc") 
+    # tokenizer = AutoTokenizer.from_pretrained("t5-small", cache_dir = "/rscratch/zhendong/yang_tasc") 
     # tokenizer = AutoTokenizer.from_pretrained("t5-3b", cache_dir = "/rscratch/zhendong/yang_tasc") 
+    tokenizer = AutoTokenizer.from_pretrained("EleutherAI/pythia-70m-deduped", revision = "step3000", cache_dir = "/rscratch/zhendong/yang_tasc") 
     
     # tokenizer = T5Tokenizer.from_pretrained("google/mt5-small") # TODO: need a better solution 
     
     # small_model = AutoModelForSeq2SeqLM.from_pretrained("t5-small", cache_dir = "/rscratch/zhendong/yang_tasc").to(torch_device) 
-    small_model = AutoModelForSeq2SeqLM.from_pretrained("t5-3b", cache_dir = "/rscratch/zhendong/yang_tasc").to(torch_device) 
+    # small_model = AutoModelForSeq2SeqLM.from_pretrained("t5-3b", cache_dir = "/rscratch/zhendong/yang_tasc").to(torch_device) 
+    small_model = GPTNeoXForCausalLM.from_pretrained("EleutherAI/pythia-6.9b", revision = "step3000", cache_dir = "/rscratch/zhendong/yang_tasc") 
     small_model.eval() 
     
     # word_prefix = "summarize: " 
-    word_prefix = "translate English to German: " 
-    word_seq = "I am new to huggingface transformers" 
+    # word_prefix = "translate English to German: " 
+    # word_seq = "I am new to huggingface transformers" 
     word_seq = "Peter want to marry a German woman" 
-    word_seq = "I am a student." 
-    word_seq = "I am currently playing with chatGPT to write a furniture assembly plan to train a robot." 
+    # word_seq = "I am a student." 
+    # word_seq = "I am currently playing with chatGPT to write a furniture assembly plan to train a robot." 
     # word_seq = "Four score and seven years ago our fathers brought forth on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal. Now we are engaged in a great civil war, testing whether that nation, or any nation so conceived and so dedicated, can long endure. We are met on a great battlefield of that war. We have come to dedicate a portion of that field, as a final resting place for those who here gave their lives that that nation might live." 
     # word_seq = "We the People of the United States, in Order to form a more perfect Union, establish Justice, insure domestic Tranquility, provide for the common defence, promote the general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do ordain and establish this Constitution for the United States of America." 
-    word_seq = "The Apollo 11 mission in 1969 marked a monumental achievement for humanity. American astronauts Neil Armstrong and Buzz Aldrin became the first humans to walk on the moon, with Armstrong's famous words: 'That's one small step for man, one giant leap for mankind." 
+    # word_seq = "The Apollo 11 mission in 1969 marked a monumental achievement for humanity. American astronauts Neil Armstrong and Buzz Aldrin became the first humans to walk on the moon, with Armstrong's famous words: 'That's one small step for man, one giant leap for mankind." 
     # word_suffix = " In the previous sentence, what did Neil Armstrong say?" 
-    word_seq = word_prefix + word_seq 
+    # word_seq = word_prefix + word_seq 
     
     input_ids = tokenizer.encode(word_seq, return_tensors = "pt").to(torch_device) 
     
@@ -124,7 +127,7 @@ def run():
     past_key_values = None 
     reporter = MemReporter(past_key_values) # monitoring memory usage 
     
-    while n < 35: 
+    while n < 10: 
         outputs = small_model(decoder_input_ids = x, encoder_outputs = encoder_outputs, past_key_values = past_key_values) 
         
         # outputs = small_model(input_ids = input_ids, decoder_input_ids = decoder_input_ids) 
