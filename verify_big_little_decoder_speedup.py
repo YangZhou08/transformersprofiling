cf155e43ff8f24c44f6e2fe108fb76144cf8f32e
YangZhou08 <yangzhou25672@utexas.edu>
2023-10-19 10:17:00 -0700

    t5tokenizer

--- verify_big_little_decoder_speedup.py
+++ verify_big_little_decoder_speedup.py
@@ -28,8 +28,8 @@ def run():
     # torch_device = 'cpu' 
     
     # from transformers import FlaxT5EncoderModel, T5Tokenizer 
-    tokenizer = AutoTokenizer("t5-small", trust_remote_code = True) 
-    # tokenizer = AutoTokenizer.from_pretrained("t5-3b", cache_dir = "/rscratch/zhendong/yang_tasc") 
+    # tokenizer = AutoTokenizer("t5-small", trust_remote_code = True) 
+    tokenizer = AutoTokenizer.from_pretrained("t5-3b", cache_dir = "/rscratch/zhendong/yang_tasc") 
     
     # tokenizer = T5Tokenizer.from_pretrained("google/mt5-small") # TODO: need a better solution 
     
