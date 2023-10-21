52bd0831544e2058d15abd407bdde7f2b2877d94
YangZhou08 <yangzhou25672@utexas.edu>
2023-10-20 18:20:39 -0700

    llmspeculativegenerate

---
+++ LLMSpeculativeGenerate.py
@@ -0,0 +1,119 @@
+import torch
+import argparse
+import contexttimer
+
+from src.transformers import AutoTokenizer, AutoModelForCausalLM 
+from torch.profiler import ProfilerActivity
+
+from sampling import autoregressive_sampling, speculative_sampling, speculative_sampling_v2
+from globals import Decoder
+
+# my local models
+MODELZOO = {
+    # https://huggingface.co/PY007/TinyLlama-1.1B-step-50K-105b
+    "llama1b": "/share_nfs/fangjiarui/root/code/hf_models/TinyLlama-1.1B-step-50K-105b",
+    "llama7b": "/share_nfs/tianzhi/code/llama-7b",
+    # https://huggingface.co/huggyllama/llama-13b
+    "llama13b": None,
+    "bloom-560m": "/share_nfs/fangjiarui/root/code/hf_models/bloom-560m",
+    "bloom7b": "/share_nfs/fangjiarui/root/code/hf_models/bloomz-7b1",
+    "baichuan-7b": "/share_nfs/duanqiyuan/models/source_models/hf/baichuan-7B",
+    "baichuan-13b": "/share_nfs/duanqiyuan/models/source_models/hf/Baichuan-13B-Base", 
+    "pythia-160M": "EleutherAI/pythia-160m", 
+    "pythia-70M": "EleutherAI/pythia-70m", 
+    "pythia-2.8b": "EleutherAI/pythia-2.8b",    
+} 
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='args for main.py')
+
+    parser.add_argument('--input', type=str, default="Suggest at least five related search terms to \"Mạng neural nhân tạo\".")
+    # parser.add_argument('--approx_model_name', type=str, default=MODELZOO["bloom-560m"]) 
+    parser.add_argument('--approx_model_name', type = str, default = MODELZOO['pythia-70M']) 
+    # parser.add_argument('--target_model_name', type=str, default=MODELZOO["bloom7b"]) 
+    parser.add_argument('--target_model_name', type = str, default = MODELZOO['pythia-2.8b']) 
+    parser.add_argument('--verbose', '-v', action='store_true', default=False, help='enable verbose mode')
+    parser.add_argument('--seed', '-s', type=int, default=None, help='set a random seed')
+    args = parser.parse_args()
+    return args
+
+
+def benchmark(fn, print_prefix, use_profiler=True, *args, **kwargs):
+    TEST_TIME = 10
+    profile_filename = f"./profile_logs/{print_prefix}"
+    
+    with contexttimer.Timer() as t:
+        if use_profiler:
+            with torch.profiler.profile(
+                activities=[torch.profiler.ProfilerActivity.CUDA],
+                schedule=torch.profiler.schedule(wait=0, warmup=1, active=2, repeat=1, skip_first=0),
+                on_trace_ready=torch.profiler.tensorboard_trace_handler(profile_filename),
+                record_shapes=False,
+                profile_memory=False,
+                # with_stack=True
+            ) as prof:
+                for _ in range(TEST_TIME): 
+                    output = fn(*args, **kwargs)
+                    prof.step()
+        else:
+            for _ in range(TEST_TIME): 
+                output = fn(*args, **kwargs)
+
+    print(f"\n [benchmark] {print_prefix}, tokens/sec: {len(output[0]) / t.elapsed / TEST_TIME}, {t.elapsed / TEST_TIME} sec generates {len(output[0])} tokens")
+
+def generate(input_text, approx_model_name, target_model_name, num_tokens=40, random_seed = None, verbose = False, use_benchmark = True):
+    # NOTE() approx_model_name and target_model_name should use the same tokenizer!
+    
+    torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    
+    tokenizer = AutoTokenizer.from_pretrained(approx_model_name, trust_remote_code=True)
+  
+    Decoder().set_tokenizer(tokenizer)
+    
+    print(f"begin loading models: \n {approx_model_name} \n {target_model_name}")
+    small_model = AutoModelForCausalLM.from_pretrained(approx_model_name, cache_dir = "/rscratch/zhendong/yang_tasc").to(torch_device) 
+    large_model = AutoModelForCausalLM.from_pretrained(target_model_name, cache_dir = "/rscratch/zhendong/yang_tasc").to(torch_device) 
+    print("finish loading models")
+    
+    input_ids = tokenizer.encode(input_text, return_tensors='pt').to(torch_device)
+
+    top_k = 10
+    top_p = 0.9
+    '''
+    torch.manual_seed(123)
+    output = autoregressive_sampling(input_ids, large_model, num_tokens, top_k = top_k, top_p=top_p)
+    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+    print(f"large (target) model autoregressive_sampling: {generated_text}")
+    
+    TEST_TIME = 10
+    if use_benchmark:
+        benchmark(autoregressive_sampling, "AS_large", True,
+                  input_ids, large_model, num_tokens, top_k = top_k, top_p=top_p)
+
+    torch.manual_seed(123)
+    output = autoregressive_sampling(input_ids, small_model, num_tokens, top_k = top_k, top_p=top_p)
+    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+    print(f"small (approx) model autoregressive_sampling: {generated_text}")
+    
+    if use_benchmark:
+        benchmark(autoregressive_sampling, "AS_small", True,
+                  input_ids, small_model, num_tokens, top_k = top_k, top_p=top_p)
+    ''' 
+    torch.manual_seed(123)
+    output = speculative_sampling_v2(input_ids, small_model, large_model, num_tokens, top_k = top_k, top_p=top_p, random_seed = random_seed)
+    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+    print(f"deepmind's speculative_sampling: {generated_text}")   
+    '''
+    torch.manual_seed(123)
+    output = speculative_sampling(input_ids, small_model, large_model, num_tokens, top_k = top_k, top_p=top_p, random_seed = random_seed, verbose = verbose)
+    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+    print(f"google's speculative_sampling: {generated_text}")
+    
+    if use_benchmark:
+        benchmark(speculative_sampling, "SP", True,
+                  input_ids, small_model, large_model, max_len = num_tokens, top_k = top_k, top_p=top_p, random_seed = random_seed)
+    ''' 
+if __name__ == "__main__":
+    args = parse_arguments()
+    
+    generate(args.input, args.approx_model_name, args.target_model_name, random_seed = args.seed, verbose=args.verbose)
