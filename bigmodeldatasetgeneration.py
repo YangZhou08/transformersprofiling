9fc49331a57d27d579ec77d47cd4f948813e2974
YangZhou08 <yangzhou25672@utexas.edu>
2023-10-24 17:01:31 -0400

    wikitext

--- bigmodeldatasetgeneration.py
+++ bigmodeldatasetgeneration.py
@@ -1,7 +1,7 @@
 import datasets 
 from datasets import load_dataset 
 
-onedataset = load_dataset("wikitext", cache_dir = "/rscratch/zhendong/yang_tasc") 
+onedataset = load_dataset("wikitext", "wikitext-103-v1", cache_dir = "/rscratch/zhendong/yang_tasc") 
 
 train_dataset = onedataset["train"] 
 validation_dataset = onedataset["validation"] 
