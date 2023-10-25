5d7d86d70c78c17ffc2144665c2c13d29bd1f6a4
YangZhou08 <yangzhou25672@utexas.edu>
2023-10-25 03:29:22 -0400

    bnb4b

--- bigmodeldatasetgeneration.py
+++ bigmodeldatasetgeneration.py
@@ -34,10 +34,17 @@ tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf", cache_dir 
 # small_model = GPTNeoXForCausalLM.from_pretrained("EleutherAI/pythia-6.9b", revision = "step3000", cache_dir = cache_dir).to(torch_device) 
 # small_model = GPTNeoXForCausalLM.from_pretrained("EleutherAI/pythia-2.8b", revision = "step3000", cache_dir = cache_dir).to(torch_device) 
 # small_model = GPTNeoXForCausalLM.from_pretrained("EleutherAI/pythia-70m-deduped", revision = "step3000", cache_dir = cache_dir).to(torch_device) 
-quant_config = BitsAndBytesConfig( 
-    load_in_8bit = True, 
-    llm_int8_has_fp16_weight = True, 
-) 
+# quant_config = BitsAndBytesConfig( 
+#     load_in_8bit = True, 
+#     llm_int8_has_fp16_weight = True, 
+# ) 
+quant_config = BitsAndBytesConfig(
+    load_in_4bit = True, 
+    llm_int4_has_fp16_weight = True, 
+    bnb_4bit_quant_type = "nf4", 
+    bnb_4bit_compute_dtype = "float16", 
+    bnb_4bit_use_double_quant = False 
+)
 small_model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf", quantization_config = quant_config).to(torch_device) 
 small_model.eval() 
 
