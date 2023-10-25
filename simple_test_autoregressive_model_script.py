29e538a398b465fe75544d14a06da03979ca016b
YangZhou08 <yangzhou25672@utexas.edu>
2023-10-24 23:45:21 -0400

    printoutmodel

--- simple_test_autoregressive_model_script.py
+++ simple_test_autoregressive_model_script.py
@@ -89,9 +89,10 @@ def run():
     tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf", cache_dir = cache_dir) 
     
     # small_model = GPTNeoXForCausalLM.from_pretrained("EleutherAI/pythia-6.9b", revision = "step3000", cache_dir = "/rscratch/zhendong/yang_tasc").to(torch_device) 
-    small_model = GPTNeoXForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf", cache_dir = cache_dir).to(torch_device) 
+    # small_model = GPTNeoXForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf", cache_dir = cache_dir).to(torch_device) 
     # small_model = GPTNeoXForCausalLM.from_pretrained("EleutherAI/pythia-70m-deduped", revision = "step3000", cache_dir = cache_dir).to(torch_device) 
-    # small_model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf", cache_dir = cache_dir).to(torch_device) 
+    small_model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf", cache_dir = cache_dir).to(torch_device) 
+    print(type(small_model)) 
     small_model.eval() 
     
     # word_prefix = "summarize: " 
