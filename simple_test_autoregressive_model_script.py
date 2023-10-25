a862e27d29166fa743623b7b3b245e18dddf06dc
YangZhou08 <yangzhou25672@utexas.edu>
2023-10-24 23:09:28 -0400

    changeo

--- simple_test_autoregressive_model_script.py
+++ simple_test_autoregressive_model_script.py
@@ -85,10 +85,11 @@ def run():
     # torch_device = 'cpu' 
     
     # tokenizer = AutoTokenizer.from_pretrained("EleutherAI/pythia-2.8b-deduped", cache_dir = cache_dir) 
-    tokenizer = AutoTokenizer.from_pretrained("EleutherAI/pythia-70m-deduped", revision = "step3000", cache_dir = cache_dir) 
+    # tokenizer = AutoTokenizer.from_pretrained("EleutherAI/pythia-70m-deduped", revision = "step3000", cache_dir = cache_dir) 
+    tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b", cache_dir = cache_dir) 
     
     # small_model = GPTNeoXForCausalLM.from_pretrained("EleutherAI/pythia-6.9b", revision = "step3000", cache_dir = "/rscratch/zhendong/yang_tasc").to(torch_device) 
-    small_model = GPTNeoXForCausalLM.from_pretrained("EleutherAI/pythia-2.8b-deduped", cache_dir = "/rscratch/zhendong/yang_tasc").to(torch_device) 
+    small_model = GPTNeoXForCausalLM.from_pretrained("meta-llama/Llama-2-7b", cache_dir = cache_dir).to(torch_device) 
     # small_model = GPTNeoXForCausalLM.from_pretrained("EleutherAI/pythia-70m-deduped", revision = "step3000", cache_dir = cache_dir).to(torch_device) 
     small_model.eval() 
     
