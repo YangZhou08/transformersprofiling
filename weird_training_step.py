1a03bae541fe527f0a3e8b46632e338137eef147
YangZhou08 <yangzhou25672@utexas.edu>
2023-11-02 12:46:39 -0400

    changeodim

--- weird_training_step.py
+++ weird_training_step.py
@@ -74,6 +74,7 @@ class CustomTrainer(Trainer):
             list_of_last_hidden_states = [token_hidden_states[-1][:, -1, :] for token_hidden_states in large_outputs.hidden_states] 
             downsampled_vectors = self.downsample_vectors(list_of_last_hidden_states) 
             assert len(downsampled_vectors) == 64/4 
+            print("each dim of downsampled_vectors is {}".format(downsampled_vectors[0].shape)) 
             downsampled_vectors = torch.cat(downsampled_vectors, dim = 1) 
             print("downsampled vector dimension is {}".format(downsampled_vectors.shape)) 
             print("shape of the downsampled vectors is {} hidden states dim {}".format(len(downsampled_vectors), downsampled_vectors[0].shape)) 
