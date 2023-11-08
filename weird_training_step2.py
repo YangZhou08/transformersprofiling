dc09ca3d9b3e614c2f9a531517c2253be1fdf365
YangZhou08 <yangzhou25672@utexas.edu>
2023-11-08 16:41:21 -0500

    disablega

--- weird_training_step2.py
+++ weird_training_step2.py
@@ -286,11 +286,11 @@ training_args = TrainingArguments(
     overwrite_output_dir=True,      
     num_train_epochs=50,            # number of training epochs, feel free to tweak
     per_device_train_batch_size=2, # the training batch size, put it as high as your GPU memory fits
-    gradient_accumulation_steps=8,  # accumulating the gradients before updating the weights
+    # gradient_accumulation_steps=8,  # accumulating the gradients before updating the weights
     per_device_eval_batch_size=64,  # evaluation batch size
     logging_steps=1000,             # evaluate, log and save model checkpoints every 1000 step
     save_steps=1000, 
-    learning_rate = 5e-4, 
+    learning_rate=5e-5, 
     # load_best_model_at_end=True,  # whether to load the best model (in terms of loss) at the end of training
     # save_total_limit=3,           # whether you don't have much space so you let only 3 model weights saved in the disk 
 ) 
