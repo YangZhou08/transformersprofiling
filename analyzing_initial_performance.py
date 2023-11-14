db864ef3415d65a4949c7b8c67d5ed2e32578d27
YangZhou08 <yangzhou25672@utexas.edu>
2023-11-14 15:03:10 -0500

    cudaoutofmem

--- analyzing_initial_performance.py
+++ analyzing_initial_performance.py
@@ -196,7 +196,7 @@ for batch in dataloader:
     else: 
         outputs = small_model(input_ids = input_ids, attention_mask = attention_mask, labels = labels) 
     loss = outputs["loss"] if isinstance(outputs, dict) else outputs[0] 
-    total_loss += loss.mean() 
+    # total_loss += loss.mean() 
     perplexity = torch.exp(loss).mean().item() 
     print(colored("perplexity is {}".format(perplexity), "yellow")) 
     print() 
