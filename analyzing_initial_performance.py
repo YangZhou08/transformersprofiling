f4fe283d8a66429f7174f5f1e66d00a76564d991
YangZhou08 <yangzhou25672@utexas.edu>
2023-11-14 15:17:10 -0500

    strangeproblem

--- analyzing_initial_performance.py
+++ analyzing_initial_performance.py
@@ -174,7 +174,8 @@ dataloader = DataLoader(datasetnew, batch_size = 8)
 # generated using GPT-4 
 # Compute perplexity over the dataset
 total_perplexity = 0 
-total_loss = torch.zeros(1).to(torch_device) 
+# total_loss = torch.zeros(1).to(torch_device) 
+total_loss = 0 
 num_batches = 0 
 count = 0 
 
@@ -196,7 +197,7 @@ for batch in dataloader:
         outputs = small_model(input_ids = input_ids, attention_mask = attention_mask, labels = labels) 
     loss = outputs["loss"] if isinstance(outputs, dict) else outputs[0] 
     print("size of loss is {}".format(loss)) 
-    total_loss += loss.mean() 
+    total_loss += loss.item() 
     perplexity = torch.exp(loss).mean().item() 
     print(colored("perplexity is {}".format(perplexity), "yellow")) 
     print() 
@@ -205,6 +206,6 @@ for batch in dataloader:
     count += 1 
 
 average_perplexity = total_perplexity / num_batches 
-reference_perplexity = torch.exp(total_loss / num_batches) 
+reference_perplexity = torch.exp(torch.Tensor(total_loss / num_batches)).item() 
 print(colored("reference perplexity is {}".format(reference_perplexity), "yellow")) 
 print("average perplexity is {}".format(average_perplexity)) 
