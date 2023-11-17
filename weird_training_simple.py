58a60037a6bb4cc46a5ff6be3c3e5232346d5fe9
YangZhou08 <yangzhou25672@utexas.edu>
2023-11-17 17:50:27 -0500

    togetherForming

--- weird_training_simple.py
+++ weird_training_simple.py
@@ -195,6 +195,7 @@ class CustomTrainer(Trainer):
         hidden_states_of_interest = hidden_states_of_interest.to(torch.float) 
         outputs = model(input_ids = large_outputs.sequences[:, :-1], attention_mask = attention_mask, added_condensed_token = hidden_states_of_interest, return_dict = True) 
         print("shape of the smll model logits: {}".format(outputs.logits.shape)) 
+        print(self.togetherForming) 
         if self.togetherForming == "average": 
             loss = torch.nn.CrossEntropyLoss()(outputs.logits[:, -1, :], large_outputs.sequences[:, -1]) 
         elif self.togetherForming == "concatenation": 
@@ -355,7 +356,7 @@ for k, v in small_model.named_parameters():
 print(colored("length of pretraining weights group is {}".format(len(pretraining_weights_group)), "red")) 
 print(colored("length of newly initialized weights group is {}".format(len(newly_initialized_group)), "red")) 
 
-if args.freeze_pretrained: 
+if args.freqeze_pretrained: 
     custom_optimizer = AdamW([
         {"params": newly_initialized_group, "lr": args.group2lr}, 
     ]) 
