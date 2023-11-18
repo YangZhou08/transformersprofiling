40350ebac0a342953761b7753406560e26d03fa4
YangZhou08 <yangzhou25672@utexas.edu>
2023-11-17 20:07:56 -0500

    changoe'

---
+++ existing_training_script.py
@@ -0,0 +1,37 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+import torch
+import torch.nn as nn
+# import bitsandbytes as bnb
+from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM
+
+model = AutoModelForCausalLM.from_pretrained(
+    "facebook/opt-125m", 
+    # load_in_8bit=True, 
+    # device_map='auto',
+) 
+
+tokenizer = AutoTokenizer.from_pretrained("facebook/opt-125m") 
+
+import transformers
+from datasets import load_dataset
+data = load_dataset("Abirate/english_quotes")
+data = data.map(lambda samples: tokenizer(samples['quote']), batched=True)
+
+trainer = transformers.Trainer(
+    model=model, 
+    train_dataset=data['train'],
+    args=transformers.TrainingArguments(
+        per_device_train_batch_size=4, 
+        gradient_accumulation_steps=4,
+        warmup_steps=100, 
+        max_steps=200, 
+        learning_rate=2e-4, 
+        fp16=True,
+        logging_steps=1, 
+        output_dir='outputs'
+    ),
+    data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False)
+)
+model.config.use_cache = False  # silence the warnings. Please re-enable for inference!
+trainer.train()
