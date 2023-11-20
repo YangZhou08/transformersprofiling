27dd1009644c15c1802ce69da1fab8ca83f7e194
YangZhou08 <yangzhou25672@utexas.edu>
2023-11-20 17:57:48 -0500

    changeo

---
+++ oversimplified_c4_dataset_downloading_script.py
@@ -0,0 +1,31 @@
+from tqdm import tqdm
+# from sampling.utils import norm_logits, sample 
+
+import torch.nn.functional as F 
+
+from src.transformers.generation.logits_process import LogitsProcessorList 
+import time 
+import numpy as np 
+
+from termcolor import colored 
+
+from src.transformers import BitsAndBytesConfig 
+from src.transformers import Trainer, TrainingArguments 
+from src.transformers import DataCollatorForLanguageModeling 
+from src.transformers.generation.utils import GenerationConfig 
+
+import os 
+import json 
+
+import torch 
+import argparse 
+# import contexttimer 
+
+import datasets 
+from datasets import load_dataset 
+
+# cache_dir = "/home/bc20/yang/transformersprofiling" 
+dir_dataset = "/home/yangzho6/c4_parts" 
+dir_models = "/home/yangzho6/model_checkpoints" 
+
+onedataset = load_dataset("c4", "en", split = "train", cache_dir = dir_dataset) 
