83eb8eb94d1be16184679a2ee853e0ad0e1d5a70
YangZhou08 <yangzhou25672@utexas.edu>
2023-11-20 02:07:35 -0500

    setting3

--- weird_training_step2.py
+++ weird_training_step2.py
@@ -471,7 +471,7 @@ trainer = CustomTrainer(
     data_collator = data_collator, 
     compute_metrics = compute_metrics, 
     optimizers = (custom_optimizer, None), 
-    experiment_setting = "setting2", 
+    experiment_setting = "setting3", 
 ) 
 
 
