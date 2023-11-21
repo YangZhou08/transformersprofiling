bc57fbd8fdb6e4115f4342d606ad1a3ff73d4f58
YangZhou08 <yangzhou25672@utexas.edu>
2023-11-21 02:44:42 -0500

    interest

--- weird_training_step2.py
+++ weird_training_step2.py
@@ -359,6 +359,7 @@ class CustomTrainer(Trainer):
         interest_token_count = torch.sum(indices_to_keep[:, 63 :].reshape(-1), dim = 0).item() # check whether 63 makes sense and make it more general if it is correct or not 
         # accuracy = accuracy_score(labels[indices_to_keep], preds[indices_to_keep]) 
         correct_words = torch.sum((preds[indices_to_keep] == labels[indices_to_keep]).view(-1), dim = 0).item() 
+        print("shape of indices_to_keep: {}".format(indices_to_keep.shape)) 
         interest_correct_count = torch.sum((preds[indices_to_keep][:, 63 :] == labels[indices_to_keep][:, 63 :]).view(-1), dim = 0).item() 
         print("correct words: {} and total words: {}".format(correct_words, total_valid_tokens)) 
         # use preds to compute f1 score 
