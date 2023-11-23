0f4736dae24e57fe8b4b9adef8a1db89bb71507b
YangZhou08 <yangzhou25672@utexas.edu>
2023-11-23 16:11:52 -0500

    10000

--- alignembedprojections.py
+++ alignembedprojections.py
@@ -244,7 +244,7 @@ except ImportError:
 if has_wandb: 
     wandb.init(project = "llm160m", name = "linearmodeltrainingalignment4") 
 
-for i in range(200): # 100 epochs 
+for i in range(10000): # 100 epochs 
     print("iteration {} out of {}".format(i, 10000)) 
     optimizer.zero_grad() 
     loss = loss_fn(layerone(large_model_embeddings), small_model_embeddings) 
