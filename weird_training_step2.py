50927a623083cba7ac875df8597ea16c7fa2b715
YangZhou08 <yangzhou25672@utexas.edu>
2023-11-24 14:03:51 -0500

    logits

--- weird_training_step2.py
+++ weird_training_step2.py
@@ -890,6 +890,9 @@ def compute_metrics(p):
     logits = p.predictions 
     labels = p.label_ids 
     # logits = logits[: -1] 
+    print("logits have shape {}".format(len(logits))) 
+    for i in range(len(logits)): 
+        print("logits[{}] has shape {}".format(i, logits[i].shape)) 
     logits = logits[:, : -1, :] 
     labels = labels[1: ] 
     probs = torch.softmax(torch.tensor(logits), dim = -1) 
