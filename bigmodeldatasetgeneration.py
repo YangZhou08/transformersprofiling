912a7ac698f150b9217513fb9e5d05a7d2aaf087
YangZhou08 <yangzhou25672@utexas.edu>
2023-11-25 17:58:17 -0500

    c4_file2

--- bigmodeldatasetgeneration.py
+++ bigmodeldatasetgeneration.py
@@ -59,7 +59,8 @@ else:
 torch_device = 'cuda' if torch.cuda.is_available() else 'cpu' 
 # onedataset = load_dataset('json', data_files = '/home/yangzho6/c4_parts/downloads/c4_file1.json', split = "train") 
 # onedataset = load_dataset('json', data_files = ['/home/beidic/yangzho6/c4_parts/downloads/c4_file1.json', '/home/beidic/yangzho6/c4_parts/downloads/c4_file2.json'], split = "train") 
-onedataset = load_dataset("json", data_files = '/home/beidic/yangzho6/c4_parts/downloads/c4_file1.json', split = "train") 
+# onedataset = load_dataset("json", data_files = '/home/beidic/yangzho6/c4_parts/downloads/c4_file1.json', split = "train") 
+onedataset = load_dataset("json", data_files = "/home/beidic/yangzho6/c4_parts/downloads/c4_file2.json", split = "train") 
 
 class CustomTrainer(Trainer): 
     def __init__(self, large_model = None, *args, **kwargs): 
@@ -79,6 +80,7 @@ class CustomTrainer(Trainer):
                 sum += listoflasthiddenstates[i] 
                 downsampled_vectors.append(sum/kernel_size) 
                 sum.mul_(0.) 
+                assert sum.view(-1).sum() == 0 
             else: 
                 sum += listoflasthiddenstates[i] 
         return downsampled_vectors 
