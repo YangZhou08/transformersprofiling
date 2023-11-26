8b6d0dbb698c650a10cbdfa7d72c700325abced1
YangZhou08 <yangzhou25672@utexas.edu>
2023-11-25 23:59:35 -0500

    1000

--- token_percentage_calculation.py
+++ token_percentage_calculation.py
@@ -161,6 +161,7 @@ trainer = Trainer(
 ) 
 
 for step, batch in enumerate(tqdm(trainer.get_train_dataloader())): 
-    print(step, datasetcust.special_token_count, datasetcust.total_token_count) 
+    if step % 1000 == 0: 
+        print(step, datasetcust.special_token_count, datasetcust.total_token_count) 
 
 print("special token percentage: {}".format(datasetcust.special_token_count / datasetcust.total_token_count)) 
