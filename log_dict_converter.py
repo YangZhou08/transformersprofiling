29827a109bfc6259240626887c4e12ec273f2b0a
Yang Zhou <yangzhou@Yangs-MacBook-Pro.local>
2023-12-05 02:49:19 -0500

    where

--- log_dict_converter.py
+++ log_dict_converter.py
@@ -127,6 +127,6 @@ for key in datadict.keys():
 ''' 
 print(datadict) 
 find = torch.tensor([[322, 372, 338]]) 
-print(torch.unique(torch.all(datadict == find, dim = 1))) 
+print(torch.where(torch.all(datadict == find, dim = 1))) 
 print(datadict.shape) 
 print(torch.unique((datadicc == datadict).view(-1))) 
