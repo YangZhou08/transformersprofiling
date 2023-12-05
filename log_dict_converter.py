6228121a5af35cecf1afb989865751e765f5157f
YangZhou08 <yangzhou25672@utexas.edu>
2023-12-04 19:06:51 -0500

    printmorekey

--- log_dict_converter.py
+++ log_dict_converter.py
@@ -20,6 +20,7 @@ def log_dict_converter(filename, preproc, tokenizer):
             assert tokenizer is not None 
             output_keys = [] 
             for key in keys: 
+                print("got here, keys are {}".format(key)) 
                 output_tokenized_keys = tokenizer(key, add_special_tokens = False, return_attention_mask = False, return_tensors = "pt") 
                 output_keys.append(output_tokenized_keys["input_ids"].squeeze(1)) 
             print("got here, length of the output_keys is {}".format(len(output_keys))) 
