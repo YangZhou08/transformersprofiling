629c14236b5a1c540d6e07b3b5952bb9d37b55f4
Yang Zhou <yangzhou@yangs-mbp-4.wifi.local.cmu.edu>
2023-12-06 23:16:14 -0500

    different

--- weird_training_chunked.py
+++ weird_training_chunked.py
@@ -377,8 +377,8 @@ class CustomTrainer(Trainer):
             idx_q = idx_q.cpu() 
             # index_one_hot = torch.zeros_like(p).index_put_(indices = idx_q, values = 1).to(torch.bool) 
             index_one_hot = torch.zeros_like(p).scatter_(3, idx_q.unsqueeze(3), 1).to(torch.bool) 
-            # p = p[idx_q] 
-            p = p[index_one_hot] 
+            p = p[idx_q] 
+            # p = p[index_one_hot] 
             # p = torch.gather(p, -1, idx_q.unsqueeze(-1)).squeeze(-1) 
             p = p.to(torch_device) 
             print("the shape of p is {}".format(p.shape)) 
