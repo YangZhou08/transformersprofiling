887a1f974684067147769652eb982fdd046da57e
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2023-12-07 07:48:01 -0500

    lookahead5

--- weird_training_chunked.py
+++ weird_training_chunked.py
@@ -877,7 +877,7 @@ trainer = CustomTrainer(
     optimizers = (custom_optimizer, None), 
     common_n_gram_list = hot_1000_3_grams, 
     use_filtered_hot_labels = False, 
-    n = 3, 
+    n = 5, 
 ) 
 
 
