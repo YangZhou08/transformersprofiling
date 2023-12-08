2fd5c7c75c5924abb748426a6535488d6b4bfb52
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2023-12-08 03:24:02 -0500

    totalaccuracycount

--- weird_training_chunked.py
+++ weird_training_chunked.py
@@ -362,8 +362,8 @@ class CustomTrainer(Trainer):
                 shift_labels.append(labels[:, i : i + original_seq_len - self.n].contiguous()) 
             shift_labels = torch.stack(shift_labels, dim = 2) # dimension (batch_size, seq_len - n, n) 
             # shift_labels[shift_labels.unsqueeze(-1).expand(-1, -1, 3)] = -100 
-            total_acc_poscount = (~(label_actual_mask.unsqueeze(-1).expand(-1, -1, self.n).to(torch.bool))).to(torch.long).view(-1).sum(dim = 0).item() 
-            # total_acc_poscount = (label_actual_mask.unsqueeze(-1).expand(-1, -1, self.n).to(torch.bool)).to(torch.long).view(-1).sum(dim = 0).item() 
+            # total_acc_poscount = (~(label_actual_mask.unsqueeze(-1).expand(-1, -1, self.n).to(torch.bool))).to(torch.long).view(-1).sum(dim = 0).item() 
+            total_acc_poscount = (label_actual_mask.unsqueeze(-1).expand(-1, -1, self.n).to(torch.bool)).to(torch.long).view(-1).sum(dim = 0).item() 
             model_output_logits2 = model_output_logits[:, :-(self.n), :, :].contiguous() 
             pred = torch.argmax(model_output_logits2, dim = -1) 
             assert pred.shape == shift_labels.shape 
