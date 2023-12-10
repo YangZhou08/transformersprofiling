00a4084e16d951a3eae38682776bcaac1f8712b7
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2023-12-10 04:44:10 -0500

    window56

--- weird_training_chunked.py
+++ weird_training_chunked.py
@@ -976,7 +976,7 @@ trainer = CustomTrainer(
     optimizers = (custom_optimizer, None), 
     common_n_gram_list = hot_1000_3_grams, 
     use_filtered_hot_labels = False, 
-    n = 5, 
+    n = 6, 
 ) 
 
 torch.autograd.set_detect_anomaly(True) 
