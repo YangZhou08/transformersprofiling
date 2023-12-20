fb35a7b1155cda503f06af3ea47abdf224edf662
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2023-12-19 22:47:21 -0800

    trying

--- weird_training_chunked.py
+++ weird_training_chunked.py
@@ -982,8 +982,8 @@ for i in range(len(train_dataset)):
         print("type of k is {} and k is {}".format(type(k), k)) 
         print("type of v is {} and v is {}".format(type(v), v)) 
 ''' 
-train_dataset = train_dataset.map(encode_with_truncation2, batched = True, num_proc = 4) 
-test_dataset = test_dataset.map(encode_with_truncation2, batched = True, num_proc = 4) 
+train_dataset = train_dataset.map(encode_with_truncation2, batched = True, num_proc = 32) 
+test_dataset = test_dataset.map(encode_with_truncation2, batched = True, num_proc = 32) 
 
 collection_verify = [] 
 for i in range(10): 
