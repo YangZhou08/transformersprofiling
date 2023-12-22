eb8032ea4117bc7faeca39b871f30b55f7410006
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2023-12-21 18:41:12 -0800

    chageo

--- observe_entropy_change_c4_large.py
+++ observe_entropy_change_c4_large.py
@@ -248,11 +248,14 @@ for i in range(20):
     print(entropy) 
     plt.figure(figsize = (20, 20)) 
     thresh = 0.4 
-    # colors = ["blue" if outputs_prob_max[i] <= thresh else "orange" for i in range(len(outputs_prob_max))] 
     colors = "orange" 
     ticks = [tokenizer.decode([input_ids[i]]) for i in range(len(input_ids))] 
-    # plt.bar(range(len(outputs_prob_max)), outputs_prob_max, width = 0.5, color = colors) 
     plt.bar(range(len(outputs_prob_max)), entropy, width = 0.5, color = colors) 
     plt.xticks(range(len(outputs_prob_max)), ticks, rotation = 90) 
     # plt.plot(range(len(outputs_prob_max)), outputs_prob_max, color = "orange", linewidth = 0.5, marker = "o", markersize = 0.5) 
     plt.savefig("outputs_prob_max_{}.png".format(i)) 
+    
+    colors = ["blue" if outputs_prob_max[i] <= thresh else "orange" for i in range(len(outputs_prob_max))] 
+    plt.bar(range(len(outputs_prob_max)), outputs_prob_max, width = 0.5, color = colors) 
+    plt.xticks(range(len(outputs_prob_max)), ticks, rotation = 90) 
+    plt.savefig("top_outputs_prob_max_{}.png".format(i)) 
