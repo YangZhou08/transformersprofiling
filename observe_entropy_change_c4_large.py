370a067721b2ec1e594822bdf1709c589d95a2c3
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2023-12-21 17:47:10 -0800

    differentcolors

--- observe_entropy_change_c4_large.py
+++ observe_entropy_change_c4_large.py
@@ -243,6 +243,8 @@ for i in range(20):
     outputs_prob_max = outputs_prob_max[0].detach().cpu().numpy() 
     print(outputs_prob_max) 
     plt.figure(figsize = (20, 20)) 
-    plt.bar(range(len(outputs_prob_max)), outputs_prob_max, width = 0.5, color = "orange") 
+    thresh = 0.4 
+    colors = ["blue" if outputs_prob_max[i] <= thresh else "range" for i in range(len(outputs_prob_max))] 
+    plt.bar(range(len(outputs_prob_max)), outputs_prob_max, width = 0.5, color = colors) 
     # plt.plot(range(len(outputs_prob_max)), outputs_prob_max, color = "orange", linewidth = 0.5, marker = "o", markersize = 0.5) 
     plt.savefig("outputs_prob_max_{}.png".format(i)) 
