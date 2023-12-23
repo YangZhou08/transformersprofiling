a93bfa87923257e1b36264b669da6a65b7f0904a
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2023-12-23 06:47:09 -0800

    changeO

--- bigmodeldatasetgeneration.py
+++ bigmodeldatasetgeneration.py
@@ -256,9 +256,10 @@ for step, inputs in enumerate(train_dataloader):
     large_outputs = large_model.generate(input_ids = input_ids, max_length = 128, do_sample = True, top_k = top_k, top_p = top_p, temperature = temperature, output_hidden_states = True, return_dict_in_generate = True) 
     # large_outputs = large_model.generate(input_ids = input_ids, max_length = 128, do_sample = True, top_k = top_k, top_p = top_p, temperature = temperature, output_hidden_states = True, return_dict_in_generate = True) 
     # tensor_file_path = os.path.join(synthesized_data_path, "ct_{}.pt".format(step)) 
-    for s in tokenizer.batch_decode(large_outputs.sequences): 
-        print(s[: max_length]) 
-        print(colored(s[max_length :], "blue")) 
+    for i in range(input_ids.shape[0]): 
+        example = large_outputs.sequences[i] 
+        print(tokenizer.decode(example[: max_length])) 
+        print(colored(tokenizer.decode(example[max_length : ]), "blue")) 
         print() 
     if step > 1: 
         exit(0) 
