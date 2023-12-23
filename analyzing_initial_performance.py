5738b2efafdb3d9f448b1aa089d2da581527d7af
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2023-12-23 15:57:17 -0800

    smallerbatchsize

--- analyzing_initial_performance.py
+++ analyzing_initial_performance.py
@@ -208,11 +208,11 @@ small_model.eval_mode = True
 # small_model = AutoModelForCausalLM.from_pretrained("facebook/opt-125m", cache_dir = dir_models).to(torch_device) 
 # large_model = LlamaForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf", cache_dir = dir_models).to(torch.bfloat16).to(torch_device) 
 large_model = LlamaForCausalLM.from_pretrained("princeton-nlp/Sheared-LLaMA-2.7B", cache_dir = dir_models).to(torch.bfloat16).to(torch_device) 
-# large_model.eval() 
+large_model.eval() 
 
 small_model = large_model 
 
-batch_size = 150 
+batch_size = 120 
 dataloader = DataLoader(datasetnew, batch_size = batch_size) 
 
 # generated using GPT-4 
