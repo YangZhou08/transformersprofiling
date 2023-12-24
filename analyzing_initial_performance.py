0de572b3b05416e41500e272a97613e606b39fab
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2023-12-23 16:09:14 -0800

    openllama

--- analyzing_initial_performance.py
+++ analyzing_initial_performance.py
@@ -208,10 +208,11 @@ small_model.eval_mode = True
 # small_model = AutoModelForCausalLM.from_pretrained("facebook/opt-125m", cache_dir = dir_models).to(torch_device) 
 # large_model = LlamaForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf", cache_dir = dir_models).to(torch.bfloat16).to(torch_device) 
 # large_model = LlamaForCausalLM.from_pretrained("princeton-nlp/Sheared-LLaMA-2.7B", cache_dir = dir_models).to(torch.bfloat16).to(torch_device) 
-# large_model.eval() 
+large_model = LlamaForCausalLM.from_pretrained("openlm-research/open_llama_3b_v2", cache_dir = dir_models).to(torch.bfloat16).to(torch_device) 
+large_model.eval() 
 
-# small_model = large_model 
-small_model = LlamaForCausalLM.from_pretrained("Cheng98/llama-160m", cache_dir = dir_models).to(torch_device) 
+small_model = large_model 
+# small_model = LlamaForCausalLM.from_pretrained("Cheng98/llama-160m", cache_dir = dir_models).to(torch_device) 
 
 batch_size = 200 
 dataloader = DataLoader(datasetnew, batch_size = batch_size) 
