88593007f66d23751502cc0a3052f057a92399bb
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2023-12-27 09:15:33 -0800

    using4g

--- largesmallmodel_dist_ddp.sh
+++ largesmallmodel_dist_ddp.sh
@@ -1,1 +1,1 @@
-accelerate launch --main_process_port 29500 --num_processes 1 --num_machines 1 largemodelfinetuning_ce.py 
+accelerate launch --main_process_port 29500 --num_processes 4 --num_machines 1 largemodelfinetuning_ce.py 
