89f9de5dbe9ee6321cea4c5ed0a4e7eb1b05a5db
Yang Zhou <yangzhou@DNa1cc030.SUNet>
2023-12-30 02:45:05 -0800

    shearedllama

--- accerate_dp_small_script.sh
+++ accerate_dp_small_script.sh
@@ -1,1 +1,1 @@
-accelerate launch --main_process_port 29501 --num_processes 2 --num_machines 1 weird_training_ddpstep.py --experiment_setting setting0 --kernel_size 7 --model_name shearedllama2_7b 
+accelerate launch --main_process_port 29503 --num_processes 2 --num_machines 1 weird_training_ddpstep.py --experiment_setting setting0 --kernel_size 7 --model_name shearedllama2_7b 
