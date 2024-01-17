50efb931707b75386f82837373112f1398db0c18
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-01-16 21:40:01 -0500

    changeo

--- meta_bash_scripts/meta_synthesized_data_generation.sh
+++ meta_bash_scripts/meta_synthesized_data_generation.sh
@@ -35,10 +35,10 @@ pip install -e .
 which python 
 cd /data/home/beidic/yang/transformersprofiling 
 
-CUDA_VISIBLE_DEVICES=0 python bigmodeldatasetgeneration.py --kernel_size 7 --model_name tinyllama --path_d 0 & 
-CUDA_VISIBLE_DEVICES=1 python bigmodeldatasetgeneration.py --kernel_size 7 --model_name tinyllama --path_d 1 & 
-CUDA_VISIBLE_DEVICES=2 python bigmodeldatasetgeneration.py --kernel_size 7 --model_name tinyllama --path_d 2 & 
-CUDA_VISIBLE_DEVICES=3 python bigmodeldatasetgeneration.py --kernel_size 7 --model_name tinyllama --path_d 3 & 
+CUDA_VISIBLE_DEVICES=0 python bigmodeldatasetgeneration.py --kernel_size 7 --model_name tinyllama --path_d 0 --batch_size 128 & 
+CUDA_VISIBLE_DEVICES=1 python bigmodeldatasetgeneration.py --kernel_size 7 --model_name tinyllama --path_d 1 --batch_size 128 & 
+CUDA_VISIBLE_DEVICES=2 python bigmodeldatasetgeneration.py --kernel_size 7 --model_name tinyllama --path_d 2 --batch_size 128 & 
+CUDA_VISIBLE_DEVICES=3 python bigmodeldatasetgeneration.py --kernel_size 7 --model_name tinyllama --path_d 3 --batch_size 128 & 
 
 # CUDA_VISIBLE_DEVICES=0 python bigmodeldatasetgeneration.py --kernel_size 7 --model_name tinyllama --topk 20 --path_d 0 & 
 # CUDA_VISIBLE_DEVICES=1 python bigmodeldatasetgeneration.py --kernel_size 7 --model_name tinyllama --topk 20 --path_d 1 & 
