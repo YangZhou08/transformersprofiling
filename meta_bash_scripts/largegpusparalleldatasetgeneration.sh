481e14c3ada65178b93ab2670202df0261e128e3
Yang Zhou <yangzhou@yangs-mbp-4.wifi.local.cmu.edu>
2024-01-19 14:58:47 -0500

    Changeo"

--- meta_bash_scripts/largegpusparalleldatasetgeneration.sh
+++ meta_bash_scripts/largegpusparalleldatasetgeneration.sh
@@ -21,7 +21,7 @@
 #SBATCH --cpus-per-task=20
 #SBATCH --gpus-per-node=8
 #SBATCH --no-requeue
-## SBATCH --array=0-11 # 12 jobs in total 
+#SBATCH --array=0-11 # 12 jobs in total 
 
 source /data/home/beidic/.bashrc
 source /data/home/beidic/miniconda/etc/profile.d/conda.sh
@@ -32,11 +32,11 @@ pip install -e .
 which python 
 
 start_time=$(date +%s)  # Record start time in seconds since the Unix epoch 
-# for i in 0 1 2 3 4 5 6 7
-# do 
-#     CUDA_VISIBLE_DEVICES=$i python bigmodeldatasetgeneration_largegpus.py --kernel_size 7 --model_name tinyllama --path_d $i --batch_size 128 --task_id $SLURM_ARRAY_TASK_ID & 
-# done 
-CUDA_VISIBLE_DEVICES=1 python bigmodeldatasetgeneration_largegpus.py --kernel_size 7 --model_name tinyllama --path_d 1 --batch_size 128 --task_id 0 & 
+for i in 1 2 3 4 5 6 7
+do 
+    CUDA_VISIBLE_DEVICES=$i python bigmodeldatasetgeneration_largegpus.py --kernel_size 7 --model_name tinyllama --path_d $((i - 1)) --batch_size 128 --task_id $SLURM_ARRAY_TASK_ID & 
+done 
+# CUDA_VISIBLE_DEVICES=1 python bigmodeldatasetgeneration_largegpus.py --kernel_size 7 --model_name tinyllama --path_d 1 --batch_size 128 --task_id 0 & 
 end_time=$(date +%s)    # Record end time in seconds since the Unix epoch 
 
 wait 
