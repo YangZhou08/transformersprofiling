5907a24916320ce7dc2192df5008bb580a43ad75
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-01-22 16:04:51 -0500

    {not

--- meta_bash_scripts/simple_check.sh
+++ meta_bash_scripts/simple_check.sh
@@ -4,11 +4,11 @@ for t in 0 1 2 3 4 5 6 7
     do 
     for i in 0 1 2 3 4 5 6 
         do 
-        echo "tensor_dir_kernel_7_$(t)_$(i) has "
-        ls /fsx-storygen/beidic/yang/c4llm_synthesized/tinyllama_topk20/tensor_dir_kernel_7_$(t)_$(i) | wc -l 
+        echo "tensor_dir_kernel_7_${t}_${i} has "
+        ls /fsx-storygen/beidic/yang/c4llm_synthesized/tinyllama_topk20/tensor_dir_kernel_7_${t}_${i} | wc -l 
         # cd /fsx-storygen/beidic/yang/c4llm_synthesized/tinyllama_topk20 
-        echo "c4synthesized_file1_kernel7_$(t)_$(i).json has " 
-        wc -l c4synthesized_file1_kernel7_$(t)_$(i).json 
+        echo "c4synthesized_file1_kernel7_${t}_${i}.json has " 
+        wc -l c4synthesized_file1_kernel7_${t}_${i}.json 
         # wc -l *.json 
         done 
     done 
