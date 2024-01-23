6697839a31d5ada4eb31f9139f7b73c3f26ef575
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-01-22 20:22:01 -0500

    changeo

--- meta_bash_scripts/script_for_combining.sh
+++ meta_bash_scripts/script_for_combining.sh
@@ -1,5 +1,5 @@
 cd /fsx-storygen/beidic/yang/c4llm_synthesized/tinyllama_topkna 
 for t in 0 1 2 3 4 5 6 7 
 do 
-    python jsonaggregate.py --task_id $t --kernel_size 7 --model_name tinyllama 
+    python /fsx-storygen/beidic/yang/transformersprofiling/jsonaggregate.py --task_id $t --kernel_size 7 --model_name tinyllama 
 done 
