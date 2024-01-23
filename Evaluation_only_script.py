544d37aae70c0897cfe1346a1d57970a1b8d9744
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-01-23 15:29:29 -0500

    commi

--- Evaluation_only_script.py
+++ Evaluation_only_script.py
@@ -1127,4 +1127,4 @@ trainer = CustomTrainer(
 
 # results = trainer.evaluate(eval_dataset = test_set) 
 # print(results) 
-small_model.save_pretrained("../model_checkpoints/llama-160m_deciphering_{}_{}_{}".format(args.model_name, args.experiment_setting, hash_of_time)) 
+small_model.save_pretrained("../model_checkpoints/llama-160m_deciphering_{}_{}_{}".format(args.model_name, args.experiment_setting, commit_hash)) 
