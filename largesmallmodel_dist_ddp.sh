037d4bb176778a677e5b9e1e7e0c169b4604e2b0
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-01-24 16:03:30 -0500

    changeo

--- largesmallmodel_dist_ddp.sh
+++ largesmallmodel_dist_ddp.sh
@@ -1,4 +1,4 @@
-accelerate launch --main_process_port 29505 --num_processes 1 --num_machines 1 largemodelfinetuning_mseplusceloss.py --large_model tinyllama --kernel_size 7 --batch_size 64 --debug --use_mse_only 
+accelerate launch --main_process_port 29505 --num_processes 1 --num_machines 1 largemodelfinetuning_mseplusceloss.py --large_model tinyllama --kernel_size 7 --batch_size 64 --debug --use_mse_loss 
 # accelerate launch --main_process_port 29505 --num_processes 2 --num_machines 1 largemodelfinetuning_mseplusceloss.py --large_model tinyllama --kernel_size 7 --batch_size 64 --debug 
 
 # accelerate launch --main_process_port 29503 --num_processes 1 --num_machines 1 largemodelfinetuning_mseplusceloss.py --use_pretrained_small_model --finetuned_small_model_checkpoint /home/beidic/yangzho6/model_checkpoints/llama-160m_deciphering_openallama3b_setting0_070600 --large_model openllama3b --kernel_size 7 
