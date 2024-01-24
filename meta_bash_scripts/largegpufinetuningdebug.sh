a30e784fd97a02ff630ce7b2505373db80a5e50a
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-01-23 21:43:08 -0500

    changebatchsize196

--- meta_bash_scripts/largegpufinetuningdebug.sh
+++ meta_bash_scripts/largegpufinetuningdebug.sh
@@ -12,4 +12,4 @@ wandb login --relogin
 which python 
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 
 
-accelerate launch --main_process_port 29505 --num_processes 8 --num_machines 1 largemodelfinetuning_mseplusceloss.py --large_model tinyllama --kernel_size 7 --batch_size 256 
+accelerate launch --main_process_port 29505 --num_processes 8 --num_machines 1 largemodelfinetuning_mseplusceloss.py --large_model tinyllama --kernel_size 7 --batch_size 196 
