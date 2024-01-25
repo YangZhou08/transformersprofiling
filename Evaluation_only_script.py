da35355133b5265f4757f7807a07db9f51cd3beb
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-01-25 01:28:31 -0500

    input_ids

--- Evaluation_only_script.py
+++ Evaluation_only_script.py
@@ -1083,8 +1083,8 @@ class CustomDataset:
                 head_token = torch.ones((1, ), dtype = torch.long) # pad with <s> bos token 
                 head_mask = torch.ones((1, ), dtype = torch.long) # attention mask starts with 1 
             item['input_ids'] = torch.cat((head_token, input_idsfull[57 :]), dim = 0) 
-            print("the shape of input_ids is {}".format(item['input_ids'].shape)) 
-            print("input ids is {}".format(item['input_ids'])) 
+            # print("the shape of input_ids is {}".format(item['input_ids'].shape)) 
+            # print("input ids is {}".format(item['input_ids'])) 
             # item['attention_mask'] = encoded_text['attention_mask'].squeeze(0) # remove the batch dimension 
             item['attention_mask'] = torch.cat((head_mask, encoded_text['attention_mask'].squeeze(0)[57 :]), dim = 0) 
         
