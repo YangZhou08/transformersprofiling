34cb320f9fac24b4052448cfa5d5297dcc1c8898
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-01-28 12:41:16 -0500

    celossonly

--- meta_bash_scripts/largegpufinetuning.sh
+++ meta_bash_scripts/largegpufinetuning.sh
@@ -39,6 +39,8 @@ which python
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 
 
 echo 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 | transformers-cli login 
+# echo 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 | huggingface-cli login 
+huggingface-cli login --token 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 
 
 # python largemodelfinetuning_mseplusceloss.py 
 # accelerate launch --main_process_port 29505 --num_processes 8 --num_machines 1 largemodelfinetuning_mseplusceloss2.py --large_model tinyllama --kernel_size 7 --use_mse_loss --freeze_small_model --use_pretrained_small_model --finetuned_small_model_checkpoint YangZhoumill/llama_160m_deciphering_tinyllama_setting0_01da4cb_hf --batch_size 128 
