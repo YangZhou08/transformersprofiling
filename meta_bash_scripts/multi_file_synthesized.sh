b99696aa8dbe12f72eb9c49b8fec633d4c009d91
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-01-29 18:43:35 -0500

    nosuchfileerrordirectoryor

--- meta_bash_scripts/multi_file_synthesized.sh
+++
@@ -1,9 +0,0 @@
-for i in 1 2 3 4 5 6 7 
-do 
-    sbatch /fsx-storygen/beidic/yang/transformersprofiling/meta_bash_scripts/single_file_synthesized.sh $i 
-done 
-# rm -rf /fsx-storygen/beidic/yang/c4llm_synthesized/tinyllama_topkna 
-# echo "all previous tinyllama_topkna files removed!" 
-
-# sbatch /fsx-storygen/beidic/yang/transformersprofiling/meta_bash_scripts/largegpusparalleldatasetgeneration.sh 
-echo "All jobs submitted!" 
