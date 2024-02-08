4de75e0f1b9ce6eac7340553b1b0f4de8741e829
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-02-08 04:16:13 -0500

    staticspli

--- largemodelfinetuning_mseplusceloss2.py
+++ largemodelfinetuning_mseplusceloss2.py
@@ -804,6 +804,12 @@ class CustomDataset:
             train_size = int(train_size * len(self)) 
         eval_size = len(self) - train_size 
         return random_split(self, [train_size, eval_size]) 
+    
+    def static_split(self, train_size): 
+        if isinstance(train_size, float): 
+            train_size = int(train_size * len(self)) 
+        eval_size = len(self) - train_size 
+        return self[: train_size], self[eval_size :] 
 
 # tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf", cache_dir = dir_models) 
 # large_tokenizer = LlamaTokenizer.from_pretrained("openlm-research/open_llama_3b_v2", cache_dir = dir_models) 
@@ -827,7 +833,8 @@ kernel_size = args.kernel_size
 # the max_length assignment is subject to change 
 max_length_lookup = {2 : 260, 3 : 259, 4 : 260, 5 : 259, 6 : 262, 7 : 260, 8 : 264} 
 datasetnew = CustomDataset(max_length = max_length_lookup[kernel_size], data_dir = dir_sdata, large_tokenizer = large_tokenizer, small_tokenizer = small_tokenizer, kernel_size = kernel_size, topk = args.topk, prompt_length = 64) 
-train_set, test_set = datasetnew.split(0.98) 
+# train_set, test_set = datasetnew.split(0.98) 
+train_set, test_set = datasetnew.static_split(0.98) 
 
 for i in range(0, 2): 
     item = train_set[i] 
