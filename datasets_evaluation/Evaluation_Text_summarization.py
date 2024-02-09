b6ff79ac5720ded2a54be4eec81ac658cacdcdcb
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-02-09 11:19:07 -0500

    f

---
+++ datasets_evaluation/Evaluation_Text_summarization.py
@@ -0,0 +1,244 @@
+# this script is mainly for evaluating different checkpoints (large + small or small)
+import torch 
+import argparse 
+# import contexttimer 
+
+import datasets 
+from datasets import load_dataset 
+import sys 
+import os 
+current_dir = os.path.dirname(__file__) 
+parent_dir = os.path.dirname(current_dir) 
+src_folder = os.path.join(parent_dir, "src") 
+sys.path.append(src_folder) 
+
+from transformers import AutoTokenizer, AutoModelForCausalLM, AutoModelForSeq2SeqLM 
+from transformers import GPTNeoXForCausalLM 
+from transformers import LlamaConfig, LlamaPreTrainedModel 
+from transformers import LlamaTokenizer 
+
+from tqdm import tqdm
+# from sampling.utils import norm_logits, sample 
+
+import torch.nn.functional as F 
+
+from transformers.generation.logits_process import LogitsProcessorList 
+import time 
+import numpy as np 
+
+from termcolor import colored 
+from transformers import Trainer, TrainingArguments 
+from torch import nn 
+from transformers import DataCollatorForLanguageModeling 
+from transformers.generation.utils import GenerationConfig 
+from transformers.models.llama.modeling_llama import LlamaForCausalLM, SimpleSmallModel 
+from transformers.models.llama.modeling_llama import LlamaCausalLMWeirdTwo 
+from transformers.modeling_utils import PreTrainedModel, load_sharded_checkpoint, unwrap_model 
+import time 
+from torch.utils.data import random_split 
+from transformers import BitsAndBytesConfig 
+from packaging import version 
+# import torch.nn.parallel.distributed.DistributedDataParallel as DDP 
+
+import datetime 
+
+from typing import TYPE_CHECKING, Any, Callable, Dict, List, Optional, Tuple, Union 
+from itertools import chain 
+
+if TYPE_CHECKING: 
+    import optuna 
+
+# # cache_dir = "/home/bc20/yang/" 
+# dir_dataset = "/home/yangzho6/c4_parts" 
+# dir_models = "/home/yangzho6/model_checkpoints2" 
+# dir_sdata = "/home/yangzho6/c4llm_synthesized/" 
+
+torch_device = 'cuda' if torch.cuda.is_available() else 'cpu' 
+
+# Set a global seed for reproducibility
+seed_value = 42
+from transformers import set_seed 
+set_seed(seed_value) 
+
+try:
+    import wandb
+    has_wandb = True
+except ImportError:
+    has_wandb = False 
+
+from transformers.utils import ( 
+    ADAPTER_CONFIG_NAME,
+    ADAPTER_SAFE_WEIGHTS_NAME,
+    ADAPTER_WEIGHTS_NAME,
+    CONFIG_NAME,
+    SAFE_WEIGHTS_INDEX_NAME,
+    SAFE_WEIGHTS_NAME,
+    WEIGHTS_INDEX_NAME,
+    WEIGHTS_NAME,
+    PushInProgress,
+    can_return_loss,
+    find_labels,
+    is_accelerate_available,
+    is_apex_available,
+    is_bitsandbytes_available,
+    is_datasets_available,
+    is_in_notebook,
+    is_ipex_available,
+    is_peft_available,
+    is_safetensors_available,
+    is_sagemaker_dp_enabled,
+    is_sagemaker_mp_enabled,
+    is_torch_compile_available,
+    is_torch_neuroncore_available,
+    is_torch_tpu_available,
+    logging,
+    strtobool,
+    is_torch_npu_available, 
+) 
+
+from transformers.trainer_pt_utils import (
+    DistributedTensorGatherer,
+    IterableDatasetShard,
+    LabelSmoother,
+    LengthGroupedSampler,
+    SequentialDistributedSampler,
+    distributed_broadcast_scalars,
+    distributed_concat,
+    find_batch_size,
+    get_dataloader_sampler,
+    get_model_param_count,
+    get_module_class_from_name,
+    get_parameter_names,
+    nested_concat,
+    nested_detach,
+    nested_numpify,
+    nested_xla_mesh_reduce,
+    reissue_pt_warnings,
+    remove_dummy_checkpoint,
+) 
+from transformers.trainer_utils import (
+    PREFIX_CHECKPOINT_DIR,
+    BestRun,
+    EvalLoopOutput,
+    EvalPrediction,
+    FSDPOption,
+    HPSearchBackend,
+    HubStrategy,
+    IntervalStrategy,
+    PredictionOutput,
+    RemoveColumnsCollator,
+    TrainerMemoryTracker,
+    TrainOutput,
+    default_compute_objective,
+    denumpify_detensorize,
+    enable_full_determinism,
+    find_executable_batch_size,
+    get_last_checkpoint,
+    has_length,
+    neftune_post_forward_hook,
+    number_of_arguments,
+    seed_worker,
+    set_seed,
+    speed_metrics,
+)
+from typing import TYPE_CHECKING, Any, Callable, Dict, List, Optional, Tuple, Union 
+from torch.utils.data import DataLoader, Dataset, RandomSampler, SequentialSampler 
+from transformers.integrations.deepspeed import deepspeed_init, deepspeed_load_checkpoint, is_deepspeed_available 
+
+if is_apex_available():
+    from apex import amp 
+
+if is_torch_tpu_available(check_device=False):
+    import torch_xla.core.xla_model as xm
+    import torch_xla.debug.metrics as met 
+
+if is_accelerate_available():
+    from accelerate import Accelerator, skip_first_batches
+    from accelerate import __version__ as accelerate_version
+    from accelerate.utils import DistributedDataParallelKwargs, GradientAccumulationPlugin
+
+    if version.parse(accelerate_version) > version.parse("0.20.3"):
+        from accelerate.utils import (
+            load_fsdp_model,
+            load_fsdp_optimizer,
+            save_fsdp_model,
+            save_fsdp_optimizer,
+        )
+    DATA_SAMPLERS = [RandomSampler]
+    if version.parse(accelerate_version) > version.parse("0.23.0"):
+        from accelerate.data_loader import SeedableRandomSampler
+
+        DATA_SAMPLERS += [SeedableRandomSampler]
+
+    if is_deepspeed_available():
+        from accelerate.utils import DeepSpeedSchedulerWrapper 
+
+import subprocess
+
+def get_git_commit_hash():
+    try:
+        # Run the git command to get the current commit hash
+        commit_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip()
+        # Decode from bytes to string
+        return commit_hash.decode('utf-8')
+    except subprocess.CalledProcessError:
+        # Handle cases where the git command fails (e.g., not a git repository)
+        return None
+
+commit_hash = get_git_commit_hash()[: 7] # only 7 digits 
+print("the commit hash is {}".format(commit_hash)) 
+
+import datetime 
+hash_of_time = str(datetime.datetime.now()).split('.')[-1] 
+print("the hash of time is {}".format(hash_of_time)) 
+
+# Name of the files used for checkpointing
+TRAINING_ARGS_NAME = "training_args.bin"
+TRAINER_STATE_NAME = "trainer_state.json"
+OPTIMIZER_NAME = "optimizer.pt"
+OPTIMIZER_NAME_BIN = "optimizer.bin"
+SCHEDULER_NAME = "scheduler.pt"
+SCALER_NAME = "scaler.pt" 
+
+import warnings 
+torch_device = 'cuda' if torch.cuda.is_available() else 'cpu' 
+
+import socket 
+
+hostname = socket.gethostname()
+print("Hostname:", hostname) 
+
+parser = argparse.ArgumentParser(
+                    prog='ProgramName',
+                    description='What the program does',
+                    epilog='Text at the bottom of help') 
+
+parser.add_argument("--model_name", type = str, default = "openllama3b") 
+parser.add_argument("--loading_from_checkpoint", type = str, default = None) 
+parser.add_argument("--kernel_size", type = int, default = 7) 
+parser.add_argument("--experiment_setting", type = str, default = "setting0") 
+parser.add_argument("--condensed_token_random", action = "store_true") 
+parser.add_argument("--task_id", type = int, default = 0) 
+
+args = parser.parse_args() 
+
+# model_name = "openllama3b" 
+model_name = args.model_name 
+
+if "lovelace" in hostname: 
+    # cache_dir = "/home/bc20/yang/transformersprofiling" 
+    dir_models = "/home/yangzho6/model_checkpoints/" 
+    # dir_sdata = "/home/yangzho6/c4llm_synthesized/" 
+    dir_sdata = "/home/yangzho6/slimpajama/SlimPajama-627B/test/chunk1/" 
+elif "ada" in hostname: 
+    # cache_dir = "/home/bc20/yang/transformersprofiling" 
+    dir_models = "/home/beidic/yangzho6/model_checkpoints/" 
+    dir_sdata = "/home/beidic/yangzho6/c4llm_synthesized/" 
+else: 
+    # cache_dir = "/home/bc20/yang/transformersprofiling" 
+    # dir_models = "/home/yangzho6/model_checkpoints/" 
+    dir_models = "/fsx-storygen/beidic/yang/model_checkpoints/" 
+    # dir_sdata = "/home/yangzho6/c4llm_synthesized/" 
+    dir_sdata = "/fsx-storygen/beidic/yang/c4llm_synthesized/" 
+
+
