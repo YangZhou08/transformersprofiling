2b997ad9d88130a51760d59d38e666af58e82670
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-02-21 18:26:51 -0500

    modification

--- datasets_evaluation/Evaluation_Language_modeling.py
+++ datasets_evaluation/Evaluation_Language_modeling.py
@@ -994,7 +994,7 @@ kernel_size = 7 # this is definitely subject to change
 # dfiles = ["example_holdout_{}.jsonl".format(i) for i in range(6282)] 
 # dfiles = [dir_sdata + "example_holdout_{}combined.jsonl".format(0)] 
 # dfiles = [dir_c4 + "c4_file1.json"] 
-dfiles = [dir_c4llmsynthesized + "c4synthesized_file1_kernel7_0.json"] 
+dfiles = [dir_c4llmsynthesized + "{}/".format("tinyllama") + "c4synthesized_file1_kernel7_0.json"] 
 datasetnew = load_dataset('json', data_files = dfiles, split = "train[:100000]") 
 # datasetnew = load_dataset('emozilla/pg19', split = "train") 
 
