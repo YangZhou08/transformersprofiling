db101d90e35a34010fb4738bfa13a8461819e9b4
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-02-21 14:04:49 -0500

    uninitialized

--- datasets_evaluation/Evaluation_Language_modeling_3.py
+++ datasets_evaluation/Evaluation_Language_modeling_3.py
@@ -772,7 +772,7 @@ else:
             # small_model = small_model.to(torch.bfloat16).to(torch_device) 
             large_model.set_msece_loss(use_mse_loss = False, ce_loss_only = True) 
             # large_model.set_addonsmallmodel(small_model) 
-            large_model.to(torch.float16).to(torch_device) 
+            large_model.to(torch.bfloat16).to(torch_device) 
             large_model.set_inference_setting("setting3") 
             large_model.set_walpha(0.5) 
             large_model.set_slidingwindowlength(sliding_window_length = args.kernel_size, addonmodel_start = args.kernel_size + 1) 
@@ -784,6 +784,13 @@ else:
             # small_model = SimpleSmallModel(small_config, hostname = hostname, sliding_window_length = args.kernel_size, target_model_dim = 2048) 
             # large_model.set_addonsmallmodel(small_model) 
             large_model.addonsmallmodel.set_criticalpath(hostname = hostname) 
+            large_model.set_msece_loss(use_mse_loss = False, ce_loss_only = True) 
+            large_model.to(torch.bfloat16).to(torch_device) 
+            large_model.set_inference_setting("setting3") 
+            large_model.set_walpha(0.5) 
+            large_model.set_slidingwindowlength(sliding_window_length = args.kernel_size, addonmodel_start = args.kernel_size + 1) 
+            large_model.set_tokenizer_bos_id(bos_id = tokenizer.bos_token_id, pad_id = tokenizer.pad_token_id) 
+            large_model.set_cosinesimilarity(False) 
             
         large_model.config.pad_token_id = tokenizer.pad_token_id 
         large_model.addonsmallmodel.config.pad_token_id = tokenizer.pad_token_id 
