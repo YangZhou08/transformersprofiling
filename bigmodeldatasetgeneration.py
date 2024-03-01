f558d17b293ee4752e305c4aefb30da266a09d78
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-03-01 00:28:36 -0500

    seqlength1

--- bigmodeldatasetgeneration.py
+++ bigmodeldatasetgeneration.py
@@ -323,7 +323,7 @@ for step, inputs in enumerate(train_dataloader):
     
     list_of_last_hidden_states = [token_hidden_states[-1][:, -1, :] for token_hidden_states in large_outputs.hidden_states] 
     if args.debug: 
-        token_hidden_states = large_outputs.hidden_states[0] 
+        token_hidden_states = large_outputs.hidden_states[1] 
         print(token_hidden_states[-1].shape) 
     exit(0) 
     downsampled_vectors = trainer.downsample_vectors(list_of_last_hidden_states, kernel_size = kernel_size) 
