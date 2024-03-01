72c83df323c44729c14a4d313fcd783e65ef87cd
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-03-01 13:24:46 -0500

    previ

--- largemodelfinetuning_precisionup.py
+++ largemodelfinetuning_precisionup.py
@@ -315,7 +315,7 @@ class CustomTrainer(Trainer):
             attention_mask = torch.ones((large_input_ids.shape[0], (large_input_ids.shape[1] - 1) // self.n + 1), dtype = torch.long).to(large_input_ids.device) 
         elif isinstance(self.model, LlamaWeirdLargeTest): 
             condensed_embeds_labels = inputs["condensed_embeds"] # (batch_size, 28, 3200) 
-            condensed_embeds_labels = condensed_embeds_labels.to(self.model.small_model_dtype) 
+            # condensed_embeds_labels = condensed_embeds_labels.to(self.model.small_model_dtype) 
         original_attention_mask = inputs["attention_mask"] # (batch_size, 203) 
         label2 = inputs["labels"] # (batch_size, 203) 
         print("shape of large_input_ids {} shape of small_input_ids {}".format(large_input_ids.shape, small_input_ids.shape)) 
