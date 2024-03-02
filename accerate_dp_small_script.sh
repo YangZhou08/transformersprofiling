935cc4857f11b16f4c39abb2bbde746819ea38f1
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-03-02 05:13:04 -0500

    setting3

--- accerate_dp_small_script.sh
+++ accerate_dp_small_script.sh
@@ -1,2 +1,2 @@
-accelerate launch --main_process_port 29511 --num_processes 2 --num_machines 1 weird_training_ddpstep2.py --experiment_setting setting3 --kernel_size 7 --model_name tinyllama --autoregressive_first_element 
+accelerate launch --main_process_port 29515 --num_processes 2 --num_machines 1 weird_training_ddpstep2.py --experiment_setting setting3 --kernel_size 7 --model_name tinyllama --autoregressive_first_element 
 # accelerate launch --main_process_port 29502 --num_processes 2 --num_machines 1 weird_training_ddpstep.py --experiment_setting setting0 --kernel_size 7 --model_name tinyllama --use_plain_model 
