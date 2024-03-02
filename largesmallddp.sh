8822717008e49ea02caa6c064245a9120dc186ac
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-03-01 19:12:17 -0500

    criticalpathset

--- largesmallddp.sh
+++
@@ -1,1 +0,0 @@
-accelerate launch --main_process_port 29505 --num_processes 2 --num_machines 1 weird_training_addinglarge.py --experiment_setting setting0 --kernel_size 7 --model_name tinyllama --use_large_model 
