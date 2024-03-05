3fa36dcfde0f0e9bef0ada434775300a1a2412a0
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-03-05 04:32:24 -0800

    strange

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -2416,7 +2416,7 @@ class LlamaWeirdLarge3(LlamaPreTrainedModel):
         # self.addonsmallmodel = None 
         small_config = LlamaConfig.from_pretrained("Cheng98/llama-160m") 
         # self.sliding_window_length = 7 
-        self.sliding_window_length = 4 
+        self.sliding_window_length = 2 
         self.addonsmallmodel = SimpleSmallModel(small_config, sliding_window_length = self.sliding_window_length, target_model_dim = self.config.hidden_size) 
         self.small_model_dtype = torch.bfloat16 
         self.use_mse_loss = False 
