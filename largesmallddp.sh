40f7188a9fbac600980003b1abcc6df2982d5b2a
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-03-06 00:14:40 -0800

    changeo

--- largesmallddp.sh
+++ largesmallddp.sh
@@ -1,1 +1,1 @@
-accelerate launch --main_process_port 29516 --num_processes 2 --num_machines 1 weird_training_addinglarge.py --experiment_setting setting3 --kernel_size 7 --model_name tinyllama --use_large_model --autoregressive_first_element --debug 
+accelerate launch --main_process_port 29516 --num_processes 2 --num_machines 1 weird_training_addinglarge.py --experiment_setting setting3 --kernel_size 7 --model_name tinyllama --use_large_model --autoregressive_first_element 
