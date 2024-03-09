2a217ae8befc81b0a565cf67cc3e5280b9bffe0a
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-03-08 16:50:52 -0800

    changeo

--- weird_training_ddpstep2.py
+++ weird_training_ddpstep2.py
@@ -1109,9 +1109,15 @@ class CustomDataset:
                     # filename = "c4synthesized_file1_kernel{}_{}.json".format(kernel_size, i) 
                     filename = "c4synthesized_file1_kernel{}_{}.json".format(kernel_size, i) 
                     dfiles.append(self.synthesize_dir + "{}/".format(model_name) + filename) 
-            else: 
+            elif "lovelace" in hostname: 
                 filename = "c4synthesized_file1_kernel{}_{}.json".format(kernel_size, 0) 
                 dfiles.append(self.synthesize_dir + "{}/".format(model_name) + filename) 
+            else: 
+                topk = None 
+                for i in range(0, 8): 
+                    # filename = "c4synthesized_file1_kernel{}_{}_combined.json".format(kernel_size, i) 
+                    filename = "c4synthesized_file1_kernel7_{}_combined.json".format(i) 
+                    dfiles.append(self.synthesize_dir + "{}_topk{}/".format(model_name, topk if topk is not None else "na") + filename) 
         else: 
             filename = "c4synthesized_file1.json" 
         self.dataset = load_dataset('json', data_files = dfiles, split = "train") 
