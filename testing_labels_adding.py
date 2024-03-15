735774b83bec8095eda223f9e2ce4a615177270a
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-03-15 18:28:09 -0400

    changeO'

--- testing_labels_adding.py
+++ testing_labels_adding.py
@@ -1,6 +1,7 @@
 import torch 
 
-labels = [[1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 3, 4, 5, 6, 7, 8]] 
+# labels = [[1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 3, 4, 5, 6, 7, 8]] 
+labels = [[0, 1, 2, 3, 4, 5, 6, 7], [0, 1, 2, 3, 4, 5, 6, 7]] 
 labels = torch.tensor(labels) 
 addonmodel_start = 2 
 sliding_window_length = 1 
