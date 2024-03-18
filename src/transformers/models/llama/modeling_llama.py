4418672981fb1d89dd30b9ae7f7d295944a87321
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-03-18 02:09:43 -0400

    changeO' git p

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -7215,7 +7215,7 @@ class SimpleSmallModel(LlamaPreTrainedModel):
                 print("start_idx: {}".format(start_idx)) 
                 print("self.sliding_window_length: {}".format(self.sliding_window_length)) 
                 print("condensed_embeds.shape[1]: {}".format(condensed_embeds.shape[1])) 
-                assert (input_ids.shape[1] - start_idx)//self.sliding_window_length == condensed_embeds.shape[1] # number of condensed tokens should have desired mapping with sequence length 
+                # assert (input_ids.shape[1] - start_idx)//self.sliding_window_length == condensed_embeds.shape[1] # number of condensed tokens should have desired mapping with sequence length 
             else: 
                 print("start_idx: {}".format(start_idx)) 
                 print(((input_ids.shape[1] - start_idx)//self.sliding_window_length) + 1) 
