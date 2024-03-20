c5f373d2d629369e5d0be45e7b7eddd38ea96b0a
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-03-20 05:52:18 -0400

    changeo

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -5049,13 +5049,17 @@ class LlamaWeirdLargeTest(LlamaPreTrainedModel):
         
         if labels is not None: 
             # selected_indices = list(range(7)) 
-            selected_indices = list(range(self.addonmodel_start - 1)) 
+            # selected_indices = list(range(self.addonmodel_start - 1)) 
+            selected_indices = list(range(self.addonmodel_start)) 
             # for i in range(7, seq_length): 
                 # if i not in mask_list_pos: 
                     # selected_indices.append(i) 
-            for i in range(self.addonmodel_start - 1, seq_length): 
-                if i not in mask_list_pos22: 
+            for i in range(self.addonmodel_start, seq_length): 
+                if i not in mask_list_pos: 
                     selected_indices.append(i) 
+            # for i in range(self.addonmodel_start - 1, seq_length): 
+            #     if i not in mask_list_pos22: 
+            #         selected_indices.append(i) 
             # selected_indices = mask_list_pos22 
             # print(colored("selected_indices {}".format(selected_indices), "red")) 
             # select and shift the logits 
@@ -7368,8 +7372,8 @@ class SimpleSmallModel(LlamaPreTrainedModel):
                     create_custom_forward(decoder_layer), hidden_states, attention_mask, position_ids
                 )
             else:
-                horizontal_bar_enabled = False 
-                # horizontal_bar_enabled = True 
+                # horizontal_bar_enabled = False 
+                horizontal_bar_enabled = True 
                 layer_outputs = decoder_layer(
                     hidden_states,
                     attention_mask=attention_mask,
