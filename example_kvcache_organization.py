939d4b1d93efdb8ec0544c3e90a68055a40f31a1
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-03-21 15:56:02 -0400

    large

--- example_kvcache_organization.py
+++ example_kvcache_organization.py
@@ -323,7 +323,7 @@ def Vanilla_Spec_nokvcache(tokenizer, target, draft, input_ids, gamma=4, max_len
         # verify_tokens = torch.cat([next_token, torch.LongTensor([generated_ids]).to(draft.device)], dim=1) 
         # verify_tokens = torch.cat([pred_token_idx, torch.LongTensor([generated_ids]).to(draft.device)], dim = 1) 
         verify_tokens = torch.cat([small_model_input_full_context, torch.LongTensor([generated_ids]).to(draft.device)], dim = 1) 
-        large_model_start_verifying_index = pred_token_idx.shape[1] - 1 
+        large_model_start_verifying_index = small_model_input_full_context.shape[1] - 1 
         print("verify_tokens: {}".format(verify_tokens.shape[1])) 
         print("large_model_start_verifying_index: {}".format(large_model_start_verifying_index)) 
         exit(0) 
