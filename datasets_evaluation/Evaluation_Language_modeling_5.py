3b25103f02c0397668bcd828070c51740ec5438b
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-03-27 15:14:25 -0400

    c415

--- datasets_evaluation/Evaluation_Language_modeling_5.py
+++ datasets_evaluation/Evaluation_Language_modeling_5.py
@@ -1057,7 +1057,8 @@ def get_dataset(datasetname, max_length):
             datasetnew = load_dataset("json", data_files = dfiles, split = "train[:10000]") 
     elif datasetname == "c4": 
         dfiles = [] 
-        filename = "c4_file1.json" 
+        # filename = "c4_file1.json" 
+        filename = "c4_file15.json" 
         dfiles.append(dir_c4 + filename) 
         datasetnew = load_dataset("json", data_files = dfiles, split = "train[:10000]") 
     elif datasetname == "pg19": 
@@ -1135,7 +1136,8 @@ def get_dataset(datasetname, max_length):
     # datasetnew = datasetnew.map(unflatten_list_func, num_proc = 8) 
     return datasetnew 
 
-dataset_list = ["c4llm_synthesized", "c4", "pg19", "cnn_dailymail", "openwebtext", "xsum"] 
+# dataset_list = ["c4llm_synthesized", "c4", "pg19", "cnn_dailymail", "openwebtext", "xsum"] 
+dataset_list = ["c4"] 
 # dataset_list = ["c4llm_synthesized"] # restricted dataset 
 ce_loss_list = [] 
 ppl_list = [] 
