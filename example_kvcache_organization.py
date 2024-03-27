5412ce0f6a797ef94bc22f4c74f3426525bde15b
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-03-26 22:33:05 -0400

    changeo

--- example_kvcache_organization.py
+++ example_kvcache_organization.py
@@ -462,10 +462,12 @@ def Vanilla_specu_dectesting3(tokenizer,
         print("target_model_logits first 100 elements: {}".format(target_model_logits.view(-1)[: 100])) 
         print("outputs.logits first 100 elements: {}".format(outputs2.logits[:, -1, :].view(-1)[: 100])) 
         # print("outputs.logits alternative: {}".format(outputs.logits[:, -2, :].view(-1)[: 100])) 
-        assert torch.allclose(target_model_logits, outputs2.logits[:, -1, :].to(torch.bfloat16))  # check if the two logits are the same 
+        # assert torch.allclose(target_model_logits, outputs2.logits[:, -1, :].to(torch.bfloat16))  # check if the two logits are the same 
         
-        expected_lmhead_logits = target_model.lm_head(outputs2.hidden_states[-1][:, -1, :]).to(torch.bfloat16) 
+        expected_lmhead_logits = target_model.lm_head(outputs2.hidden_states[-1][:, -1, :]) 
         assert torch.allclose(target_model_logits, expected_lmhead_logits) 
+        
+        assert torch.allclose(outputs2.logits[:, -1, :], expected_lmhead_logits) 
 
     count = 0
     verify_probs = []
