a2ea68c42d7ee2a5f399caa59dfa3b283c5aa515
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-03-26 22:27:47 -0400

    changeo

--- example_kvcache_organization.py
+++ example_kvcache_organization.py
@@ -456,11 +456,14 @@ def Vanilla_specu_dectesting3(tokenizer,
         assert torch.allclose(outputs2.hidden_states[-1][:, -1, :], outputs.last_hidden_states) 
         
         print("target_model_logits dtype: {}".format(target_model_logits.dtype)) 
-        print("outputs.logits dtype: {}".format(outputs.logits.dtype)) 
+        print("outputs.logits dtype: {}".format(outputs2.logits.dtype)) 
         print("target_model_logits first 100 elements: {}".format(target_model_logits.view(-1)[: 100])) 
-        print("outputs.logits first 100 elements: {}".format(outputs.logits[:, -1, :].view(-1)[: 100])) 
+        print("outputs.logits first 100 elements: {}".format(outputs2.logits[:, -1, :].view(-1)[: 100])) 
         # print("outputs.logits alternative: {}".format(outputs.logits[:, -2, :].view(-1)[: 100])) 
-        assert torch.allclose(target_model_logits, outputs.logits[:, -1, :].to(torch.bfloat16), atol = 1e-2)  # check if the two logits are the same 
+        # assert torch.allclose(target_model_logits, outputs2.logits[:, -1, :].to(torch.bfloat16))  # check if the two logits are the same 
+        
+        expected_lmhead_logits = target_model.lm_head(outputs2.hidden_states[-1][:, -1, :]) 
+        assert torch.allclose(target_model_logits, expected_lmhead_logits) 
 
     count = 0
     verify_probs = []
