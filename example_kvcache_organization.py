a43c4e8c64025b5e8c6357d017452511066eb0d4
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-03-28 16:30:08 -0400

    makingup

--- example_kvcache_organization.py
+++ example_kvcache_organization.py
@@ -770,7 +770,8 @@ def Vanilla_spec_decnokv22(tokenizer, target, draft, input_ids, gamma=4, max_len
         
         print("next_token shape {}".format(next_token.shape)) 
         print("pred_token_idx shape {}".format(pred_token_idx.shape)) 
-        pred_token_idx = pred_token_idx.unsqueeze(0) 
+        if len(pred_token_idx.shape) == 1: 
+            pred_token_idx = pred_token_idx.unsqueeze(0) 
         next_token = torch.cat([next_token, pred_token_idx], dim = 1) 
         
         exit(0) 
