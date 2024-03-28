49f67a3ab235da768bf6013634cbbadfb94d7357
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-03-27 23:50:32 -0400

    makech

--- example_kvcache_organization.py
+++ example_kvcache_organization.py
@@ -613,6 +613,154 @@ def Vanilla_spec_dectesting(tokenizer, target, model, input_ids, attention_mask,
     return acceptance_rate, draft_count 
 
 @torch.inference_mode()
+def Vanilla_spec_decnokv22(tokenizer, target, draft, input_ids, gamma=4, max_len=256, top_k=-1, top_p=0.9, temperature=0.6, verbose=False, file_path=None, attention_mask = None): 
+    '''
+    ############ Iterative Pre-fill ############
+    iter_prefill = math.ceil(input_ids.shape[1] / 100)
+    for i in (range(iter_prefill)):
+        outputs = target(
+            input_ids=input_ids[:, i*100:(i+1)*100],
+            past_key_values=target_cache,
+            use_cache=True,
+        )
+
+        outputs_draft = draft(
+            input_ids=input_ids[:, i*100:(i+1)*100],
+            past_key_values=draft_cache,
+            use_cache=True,
+        )
+    ''' 
+    outputs = target(
+        input_ids = input_ids, 
+        past_key_values = None, 
+        use_cache = False, 
+        attention_mask = attention_mask, 
+    ) 
+
+    resample_count = 0
+    accepted_count = 0
+    target_sample_count = 0
+    draft_count = 0 
+
+    next_token = sample(norm_logits(outputs.logits[:,-1,:], temperature=temperature ,top_k=top_k, top_p=top_p)) # predicting for the next token 
+    
+    if verbose: 
+        print("\n") 
+        spec_stream(input_ids, tokenizer, "black") 
+        spec_stream(next_token[0], tokenizer, 'cyan') 
+    # print("input_ids shape: {}".format(input_ids.shape)) 
+
+    n = 0
+    time1 = time.time() 
+    small_model_input_full_context = input_ids 
+    
+    ############ Spec Decoding ############
+    while n < max_len:
+        if next_token.shape == torch.Size([1]):
+            next_token = next_token.unsqueeze(0)
+        
+        pred_token_idx = next_token 
+        small_model_input_full_context = torch.cat([small_model_input_full_context, next_token], dim = 1).to(draft.device) 
+        # print("pred_token_idx: {}".format(pred_token_idx.shape)) 
+
+        speculation_probs = []
+        generated_ids = []
+
+        for _ in range(gamma):
+            outputs = draft(
+                input_ids = small_model_input_full_context, 
+                # past_key_values=draft_cache, 
+                # use_cache=True, 
+                past_key_values = None, 
+                use_cache = False, 
+                attention_mask = attention_mask, 
+            ) 
+
+            probs = norm_logits(outputs.logits[:,-1,:], temperature=temperature ,top_k=top_k, top_p=top_p)
+            pred_token_idx = sample(probs)
+            speculation_probs.append(probs[0]) 
+            # print("pred_token_idx: {} speculation_probs: {}".format(pred_token_idx, speculation_probs[0].shape)) 
+            
+            generated_ids.append(pred_token_idx.item())
+            draft_count += 1 
+
+        # verification
+        # verify_tokens = torch.cat([next_token, torch.LongTensor([generated_ids]).to(draft.device)], dim=1) 
+        # verify_tokens = torch.cat([pred_token_idx, torch.LongTensor([generated_ids]).to(draft.device)], dim = 1) 
+        verify_tokens = torch.cat([small_model_input_full_context, torch.LongTensor([generated_ids]).to(draft.device)], dim = 1) 
+        large_model_start_verifying_index = small_model_input_full_context.shape[1] - 1 
+        # print("verify_tokens: {}".format(verify_tokens.shape[1])) 
+        # print("large_model_start_verifying_index: {}".format(large_model_start_verifying_index)) 
+
+        with torch.no_grad():
+            outputs = target(
+                input_ids=verify_tokens,
+                past_key_values = None, 
+                use_cache = False, 
+            ) 
+
+        count = 0
+        verify_probs = []
+    
+        for i in range(gamma + 1): 
+            # assert outputs.logits.shape[1] == gamma + 1 
+            idx = i + large_model_start_verifying_index 
+            verify_probs.append(norm_logits(outputs.logits[:, idx, :], temperature=temperature ,top_k=top_k, top_p=top_p)[0]) 
+        # verify_probs.append(norm_logits(outputs.logits[:, -2, :], temperature = temperature, top_k = top_k, top_p = top_p)[0]) 
+        # print("length of speculation_probs: {} length of verify_probs: {}".format(len(speculation_probs), len(verify_probs))) 
+        
+        # print("verify_probs: {}".format(verify_probs[0].shape)) 
+        for i, speculation_prob, verify_prob in zip(generated_ids, speculation_probs, verify_probs[:-1]):
+            r = torch.rand(1, device = draft.device)
+
+            if r < torch.min(torch.tensor([1], device=r.device), (verify_prob[i] / speculation_prob[i])):
+                count += 1
+                accepted_count += 1
+                n += 1
+                pred_token_idx = torch.tensor([[i]]).to(draft.device)
+                if verbose:
+                    spec_stream(pred_token_idx, tokenizer, 'green') 
+
+                # if eos
+                if tokenizer.eos_token_id == i:
+                    draft_count -= gamma - count
+                    break
+
+            else:
+                resample_count += 1
+                n += 1 
+                # print("verify_prob: {}".format(verify_prob.shape)) 
+                # print("speculative_prob: {}".format(speculation_prob.shape)) 
+                verify_prob = verify_prob.unsqueeze(0) 
+                speculation_prob = speculation_prob.unsqueeze(0) 
+                pred_token_idx = sample(max_fn(verify_prob-speculation_prob)) 
+                if verbose:
+                    spec_stream(pred_token_idx, tokenizer, 'red')
+                break
+        
+        # if eos
+        if tokenizer.eos_token_id == pred_token_idx:
+            break
+
+        if count == len(generated_ids):
+            target_sample_count += 1
+            n += 1
+            pred_token_idx = sample(verify_probs[-1])
+            if verbose:
+                spec_stream(pred_token_idx, tokenizer, 'blue')
+
+        next_token = pred_token_idx
+
+    time2 = time.time()
+    acceptance_rate = accepted_count / draft_count
+    avg_tokens = accepted_count / draft_count * gamma
+    if verbose:
+        # print(f"Use {time2 - time1} sec to generate {n} tokens (now {target_cache.seq_len} tokens), Tokens/s: {n / (time2 - time1)}", flush=True) 
+        print(f"accepted rate {acceptance_rate}, avg generated tokens {avg_tokens}") 
+
+    return acceptance_rate, draft_count 
+
+@torch.inference_mode()
 def Vanilla_Spec_nokvcache(tokenizer, target, draft, input_ids, gamma=4, max_len=256, top_k=-1, top_p=0.9, temperature=0.6, verbose=False, file_path=None, attention_mask = None): 
     '''
     ############ Iterative Pre-fill ############
