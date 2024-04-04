145a68ca5f965dbcc7d2983c5dc1f7a35ae39472
Yang Zhou <yangzhou@yangs-mbp-4.wifi.local.cmu.edu>
2024-04-04 17:26:56 -0400

    usecorrecttokenizer

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -1092,14 +1092,6 @@ class LlamaModel(LlamaPreTrainedModel):
             attention_mask = attention_mask if (attention_mask is not None and 0 in attention_mask) else None
         else:
             # 4d mask is passed through the layers
-            print("seq_length: {}".format(seq_length)) # remove this line 
-            print("inputs_embeds.dtype: {}".format(inputs_embeds.dtype)) # remove this line 
-            # randomtensor = torch.full((seq_length, seq_length), torch.finfo(inputs_embeds.dtype).min, device = inputs_embeds.device) 
-            # randomtensor = torch.full((seq_length, seq_length), torch.finfo(torch.bfloat16).min, device = inputs_embeds.device) # remove this line 
-            randomtensor = torch.full((260, 260), torch.finfo(torch.float16).min, device = "cuda") 
-            # randomtensor = torch.full((260, 260), torch.finfo(torch.float32).min) 
-            print("we pass through a tensor making process") 
-            exit(0) 
             attention_mask = _prepare_4d_causal_attention_mask(
                 attention_mask, (batch_size, seq_length), inputs_embeds, past_key_values_length
             )
