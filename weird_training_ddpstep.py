92e4c40bea15e394383af766a805782289daa990
Yang Zhou <yangzhou@yangs-mbp-4.wifi.local.cmu.edu>
2024-04-04 15:26:14 -0400

    plainsmallmodel

--- weird_training_ddpstep.py
+++ weird_training_ddpstep.py
@@ -663,6 +663,7 @@ class CustomTrainer(Trainer):
             ''' 
             
         else: 
+            print("the plain small model is traiing:") 
             addedon_length = 0 
             outputs = model(
                 input_ids = input_ids, 
