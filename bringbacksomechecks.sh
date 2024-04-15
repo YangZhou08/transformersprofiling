d32f224f67efe7b288760cbae32a898687ac270f
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-04-14 21:36:56 -0400

    adigmore

---
+++ bringbacksomechecks.sh
@@ -0,0 +1,4 @@
+cd /home/yangzho6/model_checkpoints 
+scp -r zx22@terminator8.cs.rice.edu:/home/zx22/yangzho/smallmodelkernelsize4setting0checkpoint-1000 . 
+
+scp -r zx22@terminator8.cs.rice.edu:/home/zx22/yangzho/smallmodelkernelsize5setting0checkpoint-1000 . 
