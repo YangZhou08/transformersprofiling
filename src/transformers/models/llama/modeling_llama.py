ee20c63b6817cf312f622630e9f8f60f86d92bf8
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-04-16 07:54:39 -0400

    expandmaskshapes

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -8322,6 +8322,8 @@ class SimpleSmallModel(LlamaPreTrainedModel):
             expanded_attn_mask = _expand_mask(attention_mask, inputs_embeds.dtype, tgt_len = input_shape[-1]).to( #008000 
                 inputs_embeds.device 
             ) 
+            print("expanded attention mask shape {}".format(expanded_attn_mask.shape)) 
+            print("combined attention mask shape {}".format(combined_attention_mask.shape)) 
             combined_attention_mask = (
                 expanded_attn_mask if combined_attention_mask is None else expanded_attn_mask + combined_attention_mask 
             ) 
