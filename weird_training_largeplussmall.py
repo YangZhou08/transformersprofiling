52d51d5b5263ecb84ea7f458816330b289ffd0c7
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-04-24 13:09:16 -0400

    makingnew

--- weird_training_largeplussmall.py
+++ weird_training_largeplussmall.py
@@ -1054,8 +1054,8 @@ print(colored("we use large model", "cyan"))
 # set up a large model that supports the condensed token inputs 
 # large_model = LlamaWeirdLargeIterative.from_pretrained("TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T", cache_dir = dir_models).to(torch_device) 
 # large_model = LlamaWeirdLargeTest.from_pretrained("TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T", cache_dir = dir_models).to(torch_device) 
-assert args.finetuned_checkpoint is not None 
-large_model = LlamaWeirdLargeTest.from_pretrained(args.finetuned_checkpoint) 
+assert args.finetune_checkpoint is not None 
+large_model = LlamaWeirdLargeTest.from_pretrained(args.finetune_checkpoint) 
 # large_model = LlamaWeirdLargeTestmixedb.from_pretrained("TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T", cache_dir = dir_models).to(torch_device) 
 large_model.set_msece_loss(use_mse_loss = False, ce_loss_only = True) 
 large_model.set_sliding_window_length(args.kernel_size) 
