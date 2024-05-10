86c5ffffe44f10ceddce56f7ed301a8ceee61f5e
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-05-10 07:45:45 -0400

    debug

--- datasets_evaluation/Evaluation_Language_modeling_5.py
+++ datasets_evaluation/Evaluation_Language_modeling_5.py
@@ -1079,7 +1079,7 @@ training_args = TrainingArguments(
     label_names = ["labels"], 
 ) 
 
-if args.model_name == "debugging" or args.model_name == "debugging2" or args.model_name == "debugging3" or args.model_name == "debugging5": 
+if args.model_name == "debugging" or args.model_name == "debugging2" or args.model_name == "debugging3" or args.model_name == "debugging5" or args.model_name == "debugging6": 
     model = large_model 
     
 trainer = CustomTrainer( 
