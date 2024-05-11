0282a24859eef4e6461a527b89ce145b0db6241d
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-05-11 12:44:35 -0700

    usinggreedy

--- newdemocode.py
+++ newdemocode.py
@@ -252,8 +252,8 @@ for i, batch in enumerate(tqdm(trainer.get_eval_dataloader())):
                                 attention_mask = attention_mask, 
                                 max_length = 200, 
                                 return_dict_in_generate = True, 
-                                # do_sample = False, 
-                                do_sample = True, 
+                                do_sample = False, 
+                                # do_sample = True, 
                                 use_cache = True, 
         ) 
         print("input_ids shape {} output.sequences shape {}".format(input_ids.shape, output.sequences.shape)) 
