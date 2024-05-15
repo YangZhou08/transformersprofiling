16ccea8cbde87df710cbfbb0180c3adfb1251b9f
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-05-14 18:53:39 -0700

    topindices

--- src/transformers/griffin/llamastats.py
+++ src/transformers/griffin/llamastats.py
@@ -138,6 +138,7 @@ class LlamaMLP(nn.Module):
                     # print("(int_states / int_states.norm(dim=-1).unsqueeze(-1)).shape {}".format((int_states / int_states.norm(dim=-1).unsqueeze(-1)).shape)) 
                     # print("neuron_stat.shape {}".format(neuron_stat.shape)) 
                     topk_weight, topk_indices = select_neurons(neuron_stat, self.config.selection_method, k) 
+                    print("topk_indices.shape{}".format(topk_indices.shape)) 
                     if self.layer_index in [5, 15, 25]: 
                         # self.seqlenbyintermediate(topk_indices, int_states.shape, "intermediate_layer_{}_{}.png".format(self.layer_index, self.config.selection_method)) 
                         self.getdense(topk_indices, int_states.shape) 
