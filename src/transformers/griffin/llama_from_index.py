6745d0ef42132f99a3758233c8bd7f6b84530234
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-05-18 09:16:47 -0400

    makingclean

--- src/transformers/griffin/llama_from_index.py
+++ src/transformers/griffin/llama_from_index.py
@@ -104,13 +104,16 @@ from utils import select_neurons
 def get_llama_griffinIndexOff(model,  k_schedule):
     config = model.config
     for i, l in enumerate(model.model.layers):
-        new_mlp = LlamaMLP(config, k_schedule[i])
+        new_mlp = LlamaGriffinMLP(config, k_schedule[i]) 
 
         new_mlp.gate_proj = l.mlp.gate_proj
         new_mlp.up_proj = l.mlp.up_proj
         new_mlp.down_proj = l.mlp.down_proj
         new_mlp.act_fn = l.mlp.act_fn
 
+        if config.selection_method == "oracle": 
+            assert k_schedule[i] > 0.0 
+            
         if config.selection_method == 'magnitude':
             assert k_schedule[i] > 0.0
             gate_stat = l.mlp.gate_proj.weight.data.norm(dim=1)
@@ -119,7 +122,7 @@ def get_llama_griffinIndexOff(model,  k_schedule):
             _, indices = torch.topk(stat, int(stat.shape[1] * new_mlp.k_factor), dim=-1)
             new_mlp.prepare_reduced_weights(indices)
             new_mlp.mag_mask = torch.ones(stat.shape[-1], dtype=bool)
-            new_mlp.mag_mask[indices[0]] = False
+            new_mlp.mag_mask[indices[0]] = False 
 
         l.mlp = new_mlp
     
@@ -278,7 +281,9 @@ class LlamaDecoderLayer(nn.Module):
 
         self.self_attn = LlamaAttention(config=config, layer_idx=layer_idx)
 
-        self.mlp = LlamaMLP(config)
+        # self.mlp = LlamaGriffinMLP(config) 
+        self.mlp = LlamaMLP(config) 
+
         self.input_layernorm = LlamaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
         self.post_attention_layernorm = LlamaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
         
@@ -646,7 +651,7 @@ class LlamaModel(LlamaPreTrainedModel):
 
         return causal_mask
 
-class LlamaMLP(nn.Module):
+class LlamaGriffinMLP(nn.Module):
     def __init__(self, config, k_factor):
         super().__init__()
         self.config = config
@@ -658,9 +663,12 @@ class LlamaMLP(nn.Module):
         self.act_fn = F.silu
         
         self.k_factor = k_factor
-        self.mode = config.mode
-        assert self.mode in ['gen', 'class']
-
+        self.mode = config.mode 
+        self.pass_count = 0 
+        assert self.mode in ['gen', 'class'] 
+        
+    def resetpasscount(self): 
+        self.pass_count = 0 
 
     def prepare_reduced_weights(self, topk_indices):
         assert topk_indices.shape[0] == 1 # Batch size 1
@@ -692,33 +700,33 @@ class LlamaMLP(nn.Module):
                 F.linear(intermediate_states[i], down_proj_slices[i]) for i in range(self.config.pretraining_tp)
             ]
             down_proj = sum(down_proj)
-        else:
+        elif self.mode == "class": 
             k_factor = self.k_factor
-            if self.mode == 'gen':
-                if x.shape[1] > 1:
-                    int_states = self.act_fn(self.gate_proj(x)) * self.up_proj(x)
+            
+            assert self.mode == 'class' 
+            if self.pass_count == 0: 
+                int_states = self.act_fn(self.gate_proj(x)) * self.up_proj(x)
 
-                    # GRIFFIN Expert Selection
-                    if self.config.selection_method != 'magnitude' and k_factor > 0.0: ### 
-                        # print("shape of int_states {}".format(int_states.shape)) 
-                        k = int(int_states.shape[-1] * k_factor) 
-                        # print("int_states.norm(dim=-1).shape {}".format(int_states.norm(dim = -1).shape)) 
-                        # print("int_states.norm(dim=-1).unsqueeze(-1).shape {}".format(int_states.norm(dim=-1).unsqueeze(-1).shape)) 
-                        neuron_stat = ((int_states / int_states.norm(dim=-1).unsqueeze(-1))).norm(dim=1) # B, D 
-                        # print("(int_states / int_states.norm(dim=-1).unsqueeze(-1)).shape {}".format((int_states / int_states.norm(dim=-1).unsqueeze(-1)).shape)) 
-                        # print("neuron_stat.shape {}".format(neuron_stat.shape)) 
-                        topk_weight, topk_indices = select_neurons(neuron_stat, self.config.selection_method, k) 
-                        # print("topk_indices.shape {}".format(topk_indices.shape)) 
-                        self.prepare_reduced_weights(topk_indices)
-                        
-                    down_proj = self.down_proj(int_states)
+                # GRIFFIN Expert Selection
+                if self.config.selection_method != 'magnitude' and k_factor > 0.0: ### 
+                    # print("shape of int_states {}".format(int_states.shape)) 
+                    k = int(int_states.shape[-1] * k_factor) 
+                    # print("int_states.norm(dim=-1).shape {}".format(int_states.norm(dim = -1).shape)) 
+                    # print("int_states.norm(dim=-1).unsqueeze(-1).shape {}".format(int_states.norm(dim=-1).unsqueeze(-1).shape)) 
+                    neuron_stat = ((int_states / int_states.norm(dim=-1).unsqueeze(-1))).norm(dim=1) # B, D 
+                    # print("(int_states / int_states.norm(dim=-1).unsqueeze(-1)).shape {}".format((int_states / int_states.norm(dim=-1).unsqueeze(-1)).shape)) 
+                    # print("neuron_stat.shape {}".format(neuron_stat.shape)) 
+                    topk_weight, topk_indices = select_neurons(neuron_stat, self.config.selection_method, k) 
+                    # print("topk_indices.shape {}".format(topk_indices.shape)) 
+                    self.prepare_reduced_weights(topk_indices)
+                    
+                down_proj = self.down_proj(int_states) 
+                self.pass_count = 1 
 
-                else:
-                    if k_factor == 0.0:
-                        down_proj = 0 * x 
-                    else:
-                        down_proj =self.down_proj_reduced(self.act_fn(self.gate_proj_reduced(x)) * self.up_proj_reduced(x))
-
+            else:
+                down_proj =self.down_proj_reduced(self.act_fn(self.gate_proj_reduced(x)) * self.up_proj_reduced(x)) 
+                self.pass_count = 0 
+            '''
             elif self.mode == 'class':
                 assert x.shape[1] > 1
                 int_states = self.act_fn(self.gate_proj(x)) * self.up_proj(x)
@@ -732,11 +740,13 @@ class LlamaMLP(nn.Module):
                     mask.scatter_(dim=-1, index=topk_indices, src=torch.ones_like(mask))
                     int_states[:, -1] = mask * int_states[:, -1]
                     
-                else:
-                    int_states[:, -1, self.mag_mask.to(int_states.device)] = 0
                 down_proj = self.down_proj(int_states)
             else:
-                raise NotImplementedError
+                raise NotImplementedError 
+            ''' 
+        else: 
+            int_states = self.act_fn(self.gate_proj(x)) * self.up_proj(x)
+            down_proj = self.down_proj(int_states) 
         return down_proj
     
 class LlamaForCausalLMSpecializedIndex(LlamaPreTrainedModel): 
