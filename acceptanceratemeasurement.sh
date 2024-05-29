3ec5304a5ea9df73268ab763d6e39130d26984b8
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-05-29 16:45:24 -0700

    8b

---
+++ acceptanceratemeasurement.sh
@@ -0,0 +1,12 @@
+python example_kvcache_organization3.py --model_name meta-llama/Meta-Llama-3-8B --sparse 0.25 --usegriffin --datasetname gsm8k 
+python example_kvcache_organization3.py --model_name meta-llama/Meta-Llama-3-8B --sparse 0.25 --usegriffin --datasetname c4 
+python example_kvcache_organization3.py --model_name meta-llama/Meta-Llama-3-8B --sparse 0.5 --usegriffin --datasetname gsm8k 
+python example_kvcache_organization3.py --model_name meta-llama/Meta-Llama-3-8B --sparse 0.5 --usegriffin --datasetname c4 
+python example_kvcache_organization3.py --model_name meta-llama/Meta-Llama-3-8B --sparse 0.75 --usegriffin --datasetname gsm8k 
+python example_kvcache_organization3.py --model_name meta-llama/Meta-Llama-3-8B --sparse 0.75 --usegriffin --datasetname c4 
+python example_kvcache_organization3.py --model_name meta-llama/Meta-Llama-3-8B --sparse 0.25 --datasetname gsm8k 
+python example_kvcache_organization3.py --model_name meta-llama/Meta-Llama-3-8B --sparse 0.25 --datasetname c4 
+python example_kvcache_organization3.py --model_name meta-llama/Meta-Llama-3-8B --sparse 0.5 --datasetname gsm8k 
+python example_kvcache_organization3.py --model_name meta-llama/Meta-Llama-3-8B --sparse 0.5 --datasetname c4 
+python example_kvcache_organization3.py --model_name meta-llama/Meta-Llama-3-8B --sparse 0.75 --datasetname gsm8k 
+python example_kvcache_organization3.py --model_name meta-llama/Meta-Llama-3-8B --sparse 0.75 --datasetname c4 
