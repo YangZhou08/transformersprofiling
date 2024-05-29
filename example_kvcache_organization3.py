133101c0d1f1fc682b0cc67c34d51461426efa87
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-05-29 06:50:50 -0700

    basemodel

--- example_kvcache_organization3.py
+++ example_kvcache_organization3.py
@@ -361,7 +361,7 @@ if __name__ == "__main__":
     model.config.mode = "gen"
     model.config.selection_method = "topk" 
     
-    model = get_llama_griffin(model, schedule_k, notcats = False) 
+    # model = get_llama_griffin(model, schedule_k, notcats = False) 
     model.config.pad_token_id = tokenizer.pad_token_id 
     model.eval() 
     
