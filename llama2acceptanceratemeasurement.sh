1f010a055d83200718c4f98b28ed4fbeabe7f034
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-05-29 16:50:45 -0700

    modelname

---
+++ llama2acceptanceratemeasurement.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Define the parameters
+model_name="meta-llama/Llama-2-7b-hf"
+sparse_values=("0.25" "0.5" "0.75")
+datasets=("gsm8k" "c4")
+usegriffin_options=("true" "false")
+
+# Loop through the combinations and execute the commands
+for sparse in "${sparse_values[@]}"; do
+    for dataset in "${datasets[@]}"; do
+        for usegriffin in "${usegriffin_options[@]}"; do
+            command="python example_kvcache_organization3.py --modelname $model_name --sparse $sparse --datasetname $dataset" 
+            if [ "$usegriffin" = "true" ]; then
+                command="$command --usegriffin"
+            fi
+            echo $command
+            $command
+        done
+    done
+done
